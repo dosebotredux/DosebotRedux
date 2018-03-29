ccacd7517749af3a28735b13fbc4272ca684000b
Gabriel Morris <gabemorris@outlook.com>
2018-03-29 13:39:11 -0700

    Oops

--- refactor.js
+++ refactor.js
@@ -8,7 +8,6 @@ var util = require("util");
 
 client.on("ready", () => {
   console.log("DoseBot is online - beep boop");
-  message.channel.send("DoseBot is online - beep boop");
 });
 
 var prefix = "--";
