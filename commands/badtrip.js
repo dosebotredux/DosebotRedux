575f26217f955841cd5a4b2216e2775d9992b53a
Gabriel Morris <gabemorris@outlook.com>
2018-03-29 13:35:15 -0700

    Reformat

--- commands/badtrip.js
+++ commands/badtrip.js
@@ -2,7 +2,7 @@
 exports.run = (client, message, args) => {
   message.channel
   .send(
-      "Assistance: Taken drugs or have questions about taking drugs? Concerned or have any questions? Visit the TripSit Chat: https://chat.tripsit.me/\nThis Youtube channel is dedicated to sending positive vibes: https://www.youtube.com/user/TheMeditativeMind\n\nEverything is going to be okay! You are awesome, everyone here loves, cares, and supports you! Take long, slow, deep breaths and focus on each breath in and out!"
+      "**Assistance**: Taken drugs or have questions about taking drugs? Concerned or have any questions? Visit the TripSit Chat: https://chat.tripsit.me/\n\nThis Youtube channel is dedicated to sending positive vibes: https://www.youtube.com/user/TheMeditativeMind\n\n**Everything is going to be okay!** You are *awesome*, everyone here loves, cares, and supports you! **Take long, slow, deep breaths** and focus on each breath in and out!"
   )
   .catch(console.error);
 };
