6a17f77cc12e39d41fcb93f09fe69c036ce676a9
Gabriel Morris <gabemorris@outlook.com>
2018-03-29 13:05:23 -0700

    Updated help

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,6 @@
 //displays list of commands. in future should scan commands directory and display info for each programmatically/dynamically
 exports.run = (client, message, args) => {
 	message.channel.send(
-      "```Available commands: \n --about\n --help \n --psychtolerance [number of days since last dose] \n --dxmcalc [weight in pounds] \n --info [substance] \n ```"
+      "```Available commands: \n --about\n --help \n --tolerance [number of days since last dose] \n --dxmcalc [weight in pounds] \n --info [substance] \n ```"
     ).catch(console.error);
 }
