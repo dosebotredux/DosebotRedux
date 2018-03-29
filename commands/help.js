07bd901b687fb56bca775b389d4b67f5791f2c73
Gabriel Morris <gabemorris@outlook.com>
2018-03-29 14:23:53 -0700

    Fix extra new line

--- commands/help.js
+++ commands/help.js
@@ -2,7 +2,7 @@
 exports.run = (client, message, args) => {
   message.channel
   .send(
-    "```Available commands: \n --about\n --help \n --tolerance [number of days since last dose] \n --dxmcalc [weight in pounds] \n --info [substance] \n ```"
+    "```Available commands: \n --about\n --help \n --tolerance [number of days since last dose] \n --dxmcalc [weight in pounds] \n --info [substance]```"
   )
   .catch(console.error);
 };
