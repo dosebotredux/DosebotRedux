818531d2e67d35f2b7ce7fb5a1e48ff4fb23b2ee
Gabriel Morris <gabemorris@outlook.com>
2018-03-30 09:17:56 -0700

    Fixed lyrics

--- commands/geometry.js
+++ commands/geometry.js
@@ -2,7 +2,7 @@
 exports.run = (client, message, args) => {
   message.channel
   .send(
-    "So so fast, the sailing ship\nThe outer rim, the innocence\nThe lovely air\nThe wind it breathes\nIt comes\nWhen do you follow, where do you lead?\nWith a cut clean hand, a fire to feed\nLike the signs of the cosmic\nThe crush of the eye\nIt's just better in blood like the surest old sun!"
+    "So so fast, the sailing ships\nThe outer rim, the innocence\nThe lovely air\nThe wind it breathes\nIt comes\nWhen do you follow, where do you lead?\nWith unclean hand, a fire to feed\nLike the signs of the cosmic\nThe crush of the eyes\nIt's just better in blood like the surest old sun!\n\nhttps://www.youtube.com/watch?v=i4RanjlL7sg"
   )
   .catch(console.error);
 };
