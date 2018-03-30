7e9ab2a9da9e32836b6e48fef3b558d41af68531
Gabriel Morris <gabemorris@outlook.com>
2018-03-30 15:36:22 -0700

    Refactor

--- commands/dxmcalc.js
+++ commands/dxmcalc.js
@@ -11,34 +11,46 @@ exports.run = (client, message, args) => {
     parseFloat(result[result.length - 1]) * 0.8 +
     "mg to " +
     parseFloat(result[result.length - 1]) * 1.2 +
-    "mg of DXM for 1st Plateau"
-  )
-  .catch(console.error);
-  
-  message.channel
-  .send(
+    "mg of DXM for 1st Plateau\n\n" +
     parseFloat(result[result.length - 1]) * 1.75 +
     "mg to " +
     parseFloat(result[result.length - 1]) * 3.15 +
-    "mg of DXM for 2nd Plateau"
-  )
-  .catch(console.error);
-  
-  message.channel
-  .send(
+    "mg of DXM for 2nd Plateau\n\n" +
     parseFloat(result[result.length - 1]) * 3.5 +
     "mg to " +
     parseFloat(result[result.length - 1]) * 6.6 +
-    "mg of DXM for 3rd Plateau"
-  )
-  .catch(console.error);
-  
-  message.channel
-  .send(
+    "mg of DXM for 3rd Plateau\n\n" +
     parseFloat(result[result.length - 1]) * 6.6 +
     "mg to " +
     parseFloat(result[result.length - 1]) * 10 +
     "mg of DXM for 4th Plateau\n**Warning: Doses exceeding 1500mg are dangerous and even an experienced user should not consider them to be safe.**"
   )
   .catch(console.error);
+  
+  // message.channel
+  // .send(
+  //   parseFloat(result[result.length - 1]) * 1.75 +
+  //   "mg to " +
+  //   parseFloat(result[result.length - 1]) * 3.15 +
+  //   "mg of DXM for 2nd Plateau"
+  // )
+  // .catch(console.error);
+  
+  // message.channel
+  // .send(
+  //   parseFloat(result[result.length - 1]) * 3.5 +
+  //   "mg to " +
+  //   parseFloat(result[result.length - 1]) * 6.6 +
+  //   "mg of DXM for 3rd Plateau"
+  // )
+  // .catch(console.error);
+  
+  // message.channel
+  // .send(
+  //   parseFloat(result[result.length - 1]) * 6.6 +
+  //   "mg to " +
+  //   parseFloat(result[result.length - 1]) * 10 +
+  //   "mg of DXM for 4th Plateau\n**Warning: Doses exceeding 1500mg are dangerous and even an experienced user should not consider them to be safe.**"
+  // )
+  // .catch(console.error);
 };
