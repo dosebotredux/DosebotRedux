ec02eae55e4e825af57f7ebc1db45a40adf1e8ce
Gabriel Morris <gabemorris@outlook.com>
2018-04-04 12:17:45 -0700

    Heart function

---
+++ heart.js
@@ -0,0 +1,8 @@
+//Kaylee message
+exports.run = (client, message, args) => {
+  message.channel
+  .send(
+      ":black_heart::heart:️:heart:️:black_heart::heart:heart:️:black_heart:\n:heart::purple_heart::yellow_heart::purple_heart::yellow_heart::purple_heart::heart:\n:heart:️:purple_heart::yellow_heart::heart:️:yellow_heart::purple_heart::heart:️\n:black_heart::heart:️:purple_heart::yellow_heart::purple_heart::heart:️:black_heart:\n:black_heart::black_heart::heart:️:purple_heart::heart:️:black_heart::black_heart:\n:black_heart::black_heart::black_heart::heart:️:black_heart::black_heart::black_heart:"
+  )
+  .catch(console.error);
+};
