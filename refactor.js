da6028107632c2efe66f4700b928343ec79fcc2b
William LaFrance <wjlafrance@gmail.com>
2018-04-05 22:41:52 -0500

    re-arrange stuff for better working

--- refactor.js
+++ refactor.js
@@ -32,11 +32,10 @@ client.on("message", message => {
 
   if (message.content.indexOf(prefix) !== 0) return;
 
-  // This is the best way to define args. Trust me. <-- Someone else wrote this lol
+  const args = message.content.slice(prefix.length).trim().split(/ +/g)
   const commandName = args.shift().toLowerCase();
   const commandFunction = commandTable[commandName]
   if (!!commandFunction) {
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
     try {
       commandFunction.run(client, message, args)
     } catch (err) {
