13a57b38ed1c270d3bf64bb0aaddbbc6c27c353b
William LaFrance <wjlafrance@gmail.com>
2018-04-09 16:58:49 -0500

    code cleanup, fixed large section of indentation

--- commands/info.js
+++ commands/info.js
@@ -3,7 +3,7 @@
 
 exports.run = (client, message, args) => {
   const { request } = require("graphql-request");
-  
+
   var str = message.content;
   var result = str.split(" ");
   var drug = str
@@ -11,7 +11,7 @@ exports.run = (client, message, args) => {
   .replace("--info ", "", -1)
   .replace(/-/g, "", -1)
   .replace(/ /g, "", -1); //removes all symbols and puts everything in lower case so bot finds the images easier
-  
+
   // Temporary hardcoded solution to redirect problem
   if (drug === "2fdck") {
     drug = "2-Fluorodeschloroketamine";
@@ -118,109 +118,100 @@ exports.run = (client, message, args) => {
   if (drug === "dom") {
     drug = "DOM";
   }
-  
+
   if (drug != undefined) {
     console.log(drug);
-    
+
     // loads graphql query from separate file as "query" variable
     var query = require("../queries/info.js").info(drug);
-    
+
     console.log(query);
-    
+
     let substanceRequest = function(data) {
       return request("https://api.psychonautwiki.org", query).then(data => {
-      return data;
+        return data;
+      });
+    };
+
+    substanceRequest().then(function(data) {
+
+      console.log(data) // SHOW ME WHAT YOU GOT
+
+      var substance = data.substances[0];
+
+      //now that we've grabbed the substance object, we can dissect further
+      var roas = substance.roas;
+      console.log(roas);
+
+      //roa = "preferred roa"
+      //right now we grab first roa as the roa to display
+      //in future, maybe use a "preferred roas" json file with mapped substance names => roa
+      //also future, needs to be workable via a subsection command (--dosage sublingual lsd, --dosage oral lsd)
+      var roa = roas[0];
+
+      //this block cobbles together the dosage information section
+      //first check if there is dosage information for first (eventually preferred!!!) roa
+      //required, some substances (salvia for example) return null for dose object
+      if (roa.dose) {
+        var dosageMessage = buildDosageMessage(substance);
+      }
+
+      //this block cobbles together the duration information section
+      //first check if there is duration information for (eventually preferred!!!) roa
+      //required, some substances (salvia for example) return null for duration object
+
+      //fill out tolerance section if tolerance exists
+      if (substance.tolerance) {
+        var toleranceMessage = buildToleranceMessage(substance);
+      } else {
+        var toleranceMessage = "";
+      }
+
+      //HERE'S WHERE ALL THE MAGIC COMES TOGETHER
+      if (dosageMessage != undefined) {
+        var channelMessage =
+        "**" +
+        substance.name +
+        " information**\n\n" +
+        // // These are broken in the API
+        // "**Psychoactive class: **" +
+        // "insert psychoactive class\n" +
+        // "**Chemical class: **\n\n" +
+        dosageMessage +
+        "**Addiction potential: **\n```\n" +
+        substance.addictionPotential +
+        "```" +
+        toleranceMessage;
+      } else {
+        var channelMessage = "Error " + console.error;
+      }
+
+      message.channel.send(channelMessage).catch(console.error);
+
+      if (!isNaN(drug.charAt(0))) {
+        pwdrug = drug
+        .toUpperCase()
+        .replace(/ACO/g, "-AcO-")
+        .replace(/MEO/g, "-MeO-");
+      } else {
+        pwdrug = drug.charAt(0).toUpperCase() + drug.slice(1);
+      }
+
+      if (pwdrug.length == 3) pwdrug = pwdrug.toUpperCase();
+
+      if (pwdrug == "Dipt") pwdrug = "DiPT";
+      if (pwdrug == "Moxy") pwdrug = "5-MeO-MiPT";
+      if (pwdrug == "Molly") pwdrug = "MDMA";
+      if (pwdrug == "Mdma") pwdrug = "MDMA";
+
+      message.channel.send(`More information: <https://psychonautwiki.org/wiki/${pwdrug}>`)
+      .catch(console.error); //oppositely, the pwdrug must come out to have symbols and proper casing which is done with the code above
+    })
+    .catch(function(error) {
+      console.log("promise reected/errored out");
+      console.log(error);
     });
-  };
-  
-  let results = substanceRequest();
-  
-  console.log(results); // Promise { <pending> }
-  
-  results
-  .then(function(data) {
-    console.log(data.substances); //will log results.
-    
-    var substance = data.substances[0];
-    
-    //now that we've grabbed the substance object, we can dissect further
-    var roas = substance.roas;
-    console.log(roas);
-    
-    //roa = "preferred roa"
-    //right now we grab first roa as the roa to display
-    //in future, maybe use a "preferred roas" json file with mapped substance names => roa
-    //also future, needs to be workable via a subsection command (--dosage sublingual lsd, --dosage oral lsd)
-    var roa = roas[0];
-    
-    //this block cobbles together the dosage information section
-    //first check if there is dosage information for first (eventually preferred!!!) roa
-    //required, some substances (salvia for example) return null for dose object
-    if (roa.dose) {
-      var dosageMessage = buildDosageMessage(substance);
-    }
-    
-    //this block cobbles together the duration information section
-    //first check if there is duration information for (eventually preferred!!!) roa
-    //required, some substances (salvia for example) return null for duration object
-    
-    //fill out tolerance section if tolerance exists
-    if (substance.tolerance) {
-      var toleranceMessage = buildToleranceMessage(substance);
-    } else {
-      var toleranceMessage = "";
-    }
-    
-    //HERE'S WHERE ALL THE MAGIC COMES TOGETHER
-    if (dosageMessage != undefined) {
-      var channelMessage =
-      "**" +
-      substance.name +
-      " information**\n\n" +
-      // // These are broken in the API
-      // "**Psychoactive class: **" +
-      // "insert psychoactive class\n" +
-      // "**Chemical class: **\n\n" +
-      dosageMessage +
-      "**Addiction potential: **\n```\n" +
-      substance.addictionPotential +
-      "```" +
-      toleranceMessage;
-    } else {
-      var channelMessage = "Error " + console.error;
-    }
-    
-    message.channel.send(channelMessage).catch(console.error);
-    
-    if (!isNaN(drug.charAt(0))) {
-      pwdrug = drug
-      .toUpperCase()
-      .replace(/ACO/g, "-AcO-")
-      .replace(/MEO/g, "-MeO-");
-    } else {
-      pwdrug = drug.charAt(0).toUpperCase() + drug.slice(1);
-    }
-    
-    if (pwdrug.length == 3) pwdrug = pwdrug.toUpperCase();
-    
-    if (pwdrug == "Dipt") pwdrug = "DiPT";
-    if (pwdrug == "Moxy") pwdrug = "5-MeO-MiPT";
-    if (pwdrug == "Molly") pwdrug = "MDMA";
-    if (pwdrug == "Mdma") pwdrug = "MDMA";
-    
-    message.channel
-    .send(
-      "More information: <https://psychonautwiki.org/wiki/" +
-      pwdrug +
-      ">"
-    )
-    .catch(console.error); //oppositely, the pwdrug must come out to have symbols and proper casing which is done with the code above
-  })
-  .catch(function(error) {
-    console.log("promise reected/errored out");
-    console.log(error);
-  });
-}
+  }
 };
 
 // Functions
@@ -236,23 +227,23 @@ function buildToleranceMessage(substance) {
   "Baseline: " +
   substance.tolerance.zero +
   "```";
-  
+
   return toleranceMessage;
 }
 function buildDosageMessage(substance) {
   var dosageMessageArray = [];
-  
+
   for (let i = 0; i < substance.roas.length; i++) {
     dosageMessageArray[i] = substance.roas[i];
   }
   // console.log(dosageMessageArray);
-  
+
   var dosageMessage = "";
   console.log(dosageMessageArray);
   // if (dosageMessageArray[0].dose.heavy) {
   //   dosageMessage += dosageMessageArray[0].dose.heavy;
   // }
-  
+
   for (let i = 0; i < dosageMessageArray.length; i++) {
     // Dosage variables
     var dosageUnit = dosageMessageArray[i].dose.units;
@@ -261,7 +252,7 @@ function buildDosageMessage(substance) {
     var commonDosage = dosageMessageArray[i].dose.common;
     var strongDosage = dosageMessageArray[i].dose.strong;
     var heavyDosage = dosageMessageArray[i].dose.heavy;
-    
+
     // Dosage message
     // Add dosage + name
     if (dosageMessageArray[i].name) {
@@ -333,7 +324,7 @@ function buildDosageMessage(substance) {
     }
     // Duration
     var duration = dosageMessageArray[i].duration;
-    
+
     // Duration message
     // If duration isn't null
     if (!!duration) {
