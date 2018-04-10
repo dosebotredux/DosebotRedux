aa434969fbb43b56c7540db7f2b1898e8acdd901
Gabriel Morris <gabemorris@outlook.com>
2018-04-10 12:01:39 -0700

    Add ping

--- commands/graham.js
+++ commands/graham.js
@@ -2,7 +2,7 @@
 exports.run = (client, message, args) => {
   message.channel
   .send(
-    "https://i.imgur.com/xdXHnPC.jpg"
+    "<@200986075705901056> https://i.imgur.com/xdXHnPC.jpg"
   )
   .catch(console.error);
 };
