ea8012a64b2807c600b8c38a1cd8915a1464800a
Gabriel Morris <gabemorris@outlook.com>
2018-04-10 22:02:18 -0700

    Test

---
+++ commands/replication.js
@@ -0,0 +1,11 @@
+//Josie message
+exports.run = (client, message, args) => {
+  var replications = JSON.parse("https://www.reddit.com/r/replications.json");
+
+  console.log(replications);
+
+  // message.channel
+  // .send(
+  // )
+  // .catch(console.error);
+};
