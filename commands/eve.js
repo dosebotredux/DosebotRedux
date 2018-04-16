58248c367d2dd633ef5a2a4393256087684df411
Gabriel Morris <gabemorris@outlook.com>
2018-04-16 12:26:59 -0700

    Eve

---
+++ commands/eve.js
@@ -0,0 +1,8 @@
+//EVE message
+exports.run = (client, message, args) => {
+  message.channel
+  .send(
+    "Remember to drink water <@301136316635152385>\nEVE APPRECIATION DAY\nEVE APPRECIATION DAY\nEVE APPRECIATION DAY\nEVE APPRECIATION DAY\n"
+  )
+  .catch(console.error);
+};
