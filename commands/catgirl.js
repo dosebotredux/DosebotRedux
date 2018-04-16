a20fc364d2cd8a08f6382d2efe27acca134b3d20
Gabriel Morris <gabemorris@outlook.com>
2018-04-16 12:23:44 -0700

    Kaylee

---
+++ commands/catgirl.js
@@ -0,0 +1,8 @@
+//KAYLEE message
+exports.run = (client, message, args) => {
+  message.channel
+  .send(
+    "ALL HAIL THE GLORIOUS CATGIRL, <@183993728178978818>\nKAYLEE APPRECIATION DAY\nKAYLEE APPRECIATION DAY\nKAYLEE APPRECIATION DAY\nKAYLEE APPRECIATION DAY\n"
+  )
+  .catch(console.error);
+};
