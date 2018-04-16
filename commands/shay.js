98055d9cae1e3bfa6b8915139fc5cf14a4987121
Gabriel Morris <gabemorris@outlook.com>
2018-04-16 12:39:27 -0700

    Shay

---
+++ commands/shay.js
@@ -0,0 +1,8 @@
+//Shay message
+exports.run = (client, message, args) => {
+  message.channel
+  .send(
+    "SHAAAAAAAAAAAAAAYYYYYYYYYYYYYYYY, <@236819661549862923>! YOU ARE SUMMONED BY THE DRUG LORDS"
+  )
+  .catch(console.error);
+};
