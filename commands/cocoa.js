aade9f9225053670a9cda148bc63b47540be826d
Gabriel Morris <gabemorris@outlook.com>
2018-04-16 12:37:42 -0700

    Oops

--- commands/cocoa.js
+++ commands/cocoa.js
@@ -2,7 +2,7 @@
 exports.run = (client, message, args) => {
   message.channel
   .send(
-    "Long live the right hand of the revolution, <@278301453620084736>\nThe lightkeeper\nThe brilliant flame\nThe lord of the first flame"
+    "Long live the right hand of the revolution, <@278301453620084736>\nThe lightkeeper\nThe brilliant flame\nThe lord of the first ember"
   )
   .catch(console.error);
 };
