232d8cd8d1b11465823e52accaa7df0adfcb8380
Gabriel Morris <gabemorris@outlook.com>
2018-04-16 12:36:43 -0700

    Cocoa

---
+++ commands/cocoa.js
@@ -0,0 +1,8 @@
+//Cocoa message
+exports.run = (client, message, args) => {
+  message.channel
+  .send(
+    "Long live the right hand of the revolution, <@278301453620084736>\nThe lightkeeper\nThe brilliant flame\nThe lord of the first flame"
+  )
+  .catch(console.error);
+};
