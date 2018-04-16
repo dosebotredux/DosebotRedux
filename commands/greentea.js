17b1a7a20112bba1fdd5b8cda84a457c720667b2
Gabriel Morris <gabemorris@outlook.com>
2018-04-16 14:43:15 -0700

    Greentea

---
+++ commands/greentea.js
@@ -0,0 +1,8 @@
+//Cocoa message
+exports.run = (client, message, args) => {
+  message.channel
+  .send(
+    "@<260992449671856128> https://i.imgur.com/2OQ7LIS.jpg"
+  )
+  .catch(console.error);
+};
