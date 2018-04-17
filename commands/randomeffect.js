8268cd3d4228436ebb788c0c984be9086b82e199
Gabriel Morris <gabemorris@outlook.com>
2018-04-17 13:46:09 -0700

    Finish random replication function

--- commands/randomeffect.js
+++ commands/randomeffect.js
@@ -10,24 +10,31 @@ exports.run = (client, message, args) => {
   .replace("--randomeffect ", "", -1)
   .replace(/-/g, "", -1)
   
+  // define message array
   var messageContent = [];
   
+  // get random value for selecting random glossary item
   var glossaryRand = Math.floor(Math.random() * glossary.effects.length);
+  
+  // initialize random variable for finding random replication
   var replicationsRand;
   var name = glossary.effects[glossaryRand].name;
   
+  // push glossary info to message
   messageContent.push(glossary.effects[glossaryRand].name);
   messageContent.push(glossary.effects[glossaryRand].body);
   messageContent.push(glossary.effects[glossaryRand].url);
   
+  // loop through replications json to find matching effect
   for (let i = 0; i < replications.effects.length; i++) {
-    console.log(replications.effects[i].name.toLowerCase());
+    // if effect is found and has replications add to message
     if (replications.effects[i].name.toLowerCase() === name.toLowerCase()) {
       replicationsRand = Math.floor(Math.random() * replications.effects[i].replications.length)
       messageContent.push("Replication: " + replications.effects[i].replications[replicationsRand].url)
     }
   }
   
+  // if has replications construct message
   if (messageContent[3]) {
     message.channel
     .send("**" + messageContent[0] + "**" + 
@@ -39,6 +46,7 @@ exports.run = (client, message, args) => {
     messageContent[3]) 
     .catch(console.error);
   } else {
+    // if no replications construct message
     message.channel
     .send("**" + messageContent[0] + "**" + 
     "\n" + 
