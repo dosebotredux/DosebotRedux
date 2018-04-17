ee3d9e1ccdbaf58060454241fbf9f3d953d36fb0
Gabriel Morris <gabemorris@outlook.com>
2018-04-17 09:56:14 -0700

    Initial glossary test commit

---
+++ commands/glossary.js
@@ -0,0 +1,17 @@
+//grabs glossary info from local json and builds message based on arg
+const glossary = require("../glossary.json");
+
+exports.run = (client, message, args) => {
+  var str = message.content;
+  var result = str.split(" ");
+  console.log("str: " + str);
+  console.log("result: " + result);
+
+  for (let i = 0; i < glossary.length; i++) {
+    console.log(glossary[i].name);
+  }
+
+  message.channel
+  .send("ヽ༼ຈل͜ຈ༽ﾉ raise your dongers ヽ༼ຈل͜ຈ༽ﾉ")
+  .catch(console.error);
+};
