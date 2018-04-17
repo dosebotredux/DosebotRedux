5a113bf13915824d209d22585a54157570d2e411
Gabriel Morris <gabemorris@outlook.com>
2018-04-17 16:11:08 -0700

    Alexis

---
+++ commands/alexis.js
@@ -0,0 +1,8 @@
+//Alexis message
+exports.run = (client, message, args) => {
+    message.channel
+    .send(
+        "<@284095754686038016> Alexis is okay I guess"
+    )
+    .catch(console.error);
+};
