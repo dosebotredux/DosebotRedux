4350817a02c384f953f64e954105e124ee0dd562
Gabriel Morris <gabemorris@outlook.com>
2018-04-17 12:17:07 -0700

    Extract from scope

--- commands/effectinfo.js
+++ commands/effectinfo.js
@@ -24,16 +24,20 @@ exports.run = (client, message, args) => {
     }
   }
 
+  var rand;
+
   for (let i = 0; i < replications.effects.length; i++) {
     if (replications.effects[i].name.toLowerCase() === effect) {
       // messageContent.push(replications.effects[i].replications[0].url)
       console.log(replications.effects[i].replications[0].url);
 
-      var rand = Math.floor(Math.random() * replications.effects[i].replications.length);
+      rand = Math.floor(Math.random() * replications.effects[i].replications.length);
 
-      console.log(replications.effects[i].replications[rand].url);
     }
   }
+
+  console.log(replications.effects[i].replications[rand].url);
+
   if (messageContent[0] !== undefined) {
   message.channel
   .send("**" + messageContent[0] + "**" + 
