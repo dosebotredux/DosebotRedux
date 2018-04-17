bc9e6f18609da5783fe6e017bd4fc5c8801d44b3
Gabriel Morris <gabemorris@outlook.com>
2018-04-17 11:06:16 -0700

    Update help with new commands

--- commands/help.js
+++ commands/help.js
@@ -2,7 +2,7 @@
 exports.run = (client, message, args) => {
   message.channel
   .send(
-    "```Available commands: \n --about\n --help \n --psytolerance [number of days since last dose] \n --info [substance]\n --effects [substance]\n --dxmcalc [weight in pounds]```"
+    "```Available commands: \n --about\n --help \n --psytolerance [number of days since last dose] \n --info [substance]\n --effects [substance]\n --effectinfo [effect] \n --randomeffect \n --dxmcalc [weight in pounds]```"
   )
   .catch(console.error);
 };
---
+++ commands/memes.js
@@ -0,0 +1,8 @@
+//displays list of memes. in future should scan commands directory and display info for each programmatically/dynamically
+exports.run = (client, message, args) => {
+  message.channel
+  .send(
+    "```Available memes: \n --catgirl \n --cocoa \n --dong \n --eve \n --frogmodai \n --geometry \n --graham \n --greentea \n --heart \n --josie \n --kaylee \n --memes \n --shay \n --uwotm8```"
+  )
+  .catch(console.error);
+};
