3b500d126c8f1660504229c3df0bc567caaabdba
Gabriel Morris <gabemorris@outlook.com>
2018-04-17 10:01:46 -0700

    Reee

--- commands/glossary.js
+++ commands/glossary.js
@@ -4,16 +4,21 @@ const glossary = require("../glossary.json");
 exports.run = (client, message, args) => {
   var str = message.content;
   var result = str.split(" ");
-  var effect = result
-    .toLowerCase()
-    .replace("--glossary ", "", -1);
+  // var effect = result
+  //   .toLowerCase()
+  //   .replace("--glossary ", "", -1);
+  var drug = str
+  .toLowerCase()
+  .replace("--glossary ", "", -1)
+  .replace(/-/g, "", -1)
+  .replace(/ /g, "", -1); //removes all symbols and puts everything in lower case so bot finds the images easier
   console.log("str: " + str);
-  console.log("result: " + result);
-
+  console.log("result: " + drug);
+  
   for (let i = 0; i < glossary.length; i++) {
     console.log(glossary[i].name);
   }
-
+  
   message.channel
   .send("ヽ༼ຈل͜ຈ༽ﾉ raise your dongers ヽ༼ຈل͜ຈ༽ﾉ")
   .catch(console.error);
