fe460ee5a26b76d0355da2c22ddc1325d350233d
Gabriel Morris <gabemorris@outlook.com>
2018-04-17 10:45:11 -0700

    Random effect info

---
+++ commands/randomeffect.js
@@ -0,0 +1,36 @@
+//grabs glossary info from local json and builds message based on random effect
+const glossary = require("../glossary.json");
+
+exports.run = (client, message, args) => {
+  var str = message.content;
+  var result = str.split(" ");
+  var effect = str
+  .toLowerCase()
+  .replace("--randomeffect ", "", -1)
+  .replace(/-/g, "", -1)
+  console.log("str: " + str);
+  console.log("result: " + effect);
+  var messageContent = [];
+
+  console.log(glossary.effects.length);
+
+  console.log(Math.random * glossary.effects.length);
+  
+  for (let i = 0; i < glossary.effects.length; i++) {
+    if (glossary.effects[i].name.toLowerCase() === effect) {
+      messageContent.push(glossary.effects[i].name);
+      messageContent.push(glossary.effects[i].body);
+      messageContent.push(glossary.effects[i].url);
+
+      console.log(messageContent);
+    }
+  }
+  
+  message.channel
+  .send("**" + messageContent[0] + "**" + 
+  "\n" + 
+  "```" + messageContent[1] + "```" +
+  "\n" + 
+  "More information: " + messageContent[2])
+  .catch(console.error);
+};
