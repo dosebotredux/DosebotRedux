c016c98ebdd1f81ec946fd8e54409a29a7498f9d
Gabriel Morris <gabemorris@outlook.com>
2018-04-17 19:21:28 -0700

    Test

--- commands/dxmcalc.js
+++ commands/dxmcalc.js
@@ -4,7 +4,7 @@ exports.run = (client, message, args) => {
   var str = message.content;
   var result = str.split(" ");
   var weight = parseFloat(result[result.length - 1]);
-
+  
   var threshold = 100;
   var lightMin = 100;
   var lightMax = 200;
@@ -15,10 +15,10 @@ exports.run = (client, message, args) => {
   var heavy = 700;
   
   var calculatedDoseModifier = 2 * getLog(125, weight) - 1;
-
+  
   console.log(calculatedDoseModifier);
   console.log(result);
-
+  
   threshold *= calculatedDoseModifier;
   lightMin *= calculatedDoseModifier;
   lightMax *= calculatedDoseModifier;
@@ -27,7 +27,7 @@ exports.run = (client, message, args) => {
   strongMin *= calculatedDoseModifier;
   strongMax *= calculatedDoseModifier;
   heavy *= calculatedDoseModifier;
-
+  
   threshold = Math.floor(threshold);
   lightMin = Math.floor(lightMin);
   lightMax = Math.floor(lightMax);
@@ -36,33 +36,41 @@ exports.run = (client, message, args) => {
   strongMin = Math.floor(strongMin);
   strongMax = Math.floor(strongMax);
   heavy = Math.floor(heavy);
-
+  
   function getLog(x, y) {
     return Math.log(y) / Math.log(x);
   }
   
-  message.channel
-  .send(
-    "**DoseBot DXM calculator recommends:**" +
-    "\n" +
-    "```" +
-    "\n" +
-    "Threshold: " + threshold + "mg" +
-    "\n" +
-    "1st plateau: " + lightMin + " - " + lightMax + "mg" +
-    "\n" +
-    "2nd plateau: " + commonMin + " - " + commonMax + "mg" +
-    "\n" +
-    "3rd plateau: " + strongMin + " - " + strongMax + "mg" +
-    "\n" +
-    "4th plateau: " + heavy + "mg" +
-    "\n" +
-    "```" +
-    "\n" +
-    "**Warning:** These recommendations are an approximation, please take into account your own personal tolerance and start with lower dosages. Doses exceeding 1500mg are potentially fatal."
-  )
-  .catch(console.error);
-
+  if (threshold !== NaN) {
+    message.channel
+    .send(
+      "**DoseBot DXM calculator recommends:**" +
+      "\n" +
+      "```" +
+      "\n" +
+      "Threshold: " + threshold + "mg" +
+      "\n" +
+      "1st plateau: " + lightMin + " - " + lightMax + "mg" +
+      "\n" +
+      "2nd plateau: " + commonMin + " - " + commonMax + "mg" +
+      "\n" +
+      "3rd plateau: " + strongMin + " - " + strongMax + "mg" +
+      "\n" +
+      "4th plateau: " + heavy + "+mg" +
+      "\n" +
+      "```" +
+      "\n" +
+      "**Warning:** These recommendations are an approximation, please take into account your own personal tolerance and start with lower dosages. Doses exceeding 1500mg are potentially fatal."
+    )
+    .catch(console.error);
+  } else {
+    message.channel
+    .send(
+      "**Error:** No weight provided"
+    )
+    .catch(console.error);
+  }
+  
   // message.channel
   // .send(
   //   parseFloat(result[result.length - 1]) * 0.8 +
