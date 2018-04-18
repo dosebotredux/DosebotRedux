a9cd256230a85ee3657cd6949435043e54c31a76
William LaFrance <wjlafrance@gmail.com>
2018-04-17 22:01:27 -0500

    if what they say is true you are a shadow in the fourth dimension

---
+++ commands/fourthdimension.js
@@ -0,0 +1,5 @@
+exports.run = (client, message, args) => {
+  message.channel
+    .send("_Either there's a purpose or I'm headed out at breakfast.\nTake a drink. Take a drag.\nOne more coffee. Ugly hat.\nNo more mirrors. Woolen bag.\nAnd I am gone._")
+    .catch(console.error)
+}
