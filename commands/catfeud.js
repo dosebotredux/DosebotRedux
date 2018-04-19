79c7b4cd4beee2b90565836e8bd34956d7edc534
Gabriel Morris <gabemorris@outlook.com>
2018-04-18 17:56:16 -0700

    Catfeud

---
+++ commands/catfeud.js
@@ -0,0 +1,8 @@
+//triggered kaylee
+exports.run = (client, message, args) => {
+  message.channel
+  .send(
+    "<@183993728178978818> https://i.imgur.com/YSJEVXT.png"
+  )
+  .catch(console.error);
+};
