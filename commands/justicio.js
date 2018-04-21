c6027b155b50ed052777bd1456295545d80017e9
Gabriel Morris <gabemorris@outlook.com>
2018-04-20 18:42:17 -0700

    justice command

---
+++ commands/justicio.js
@@ -0,0 +1,8 @@
+//justice
+exports.run = (client, message, args) => {
+  message.channel
+  .send(
+    "https://i.imgur.com/uDkFxgf.jpg\n***THIS CHANNEL IS UNACCEPTABLE***"
+  )
+  .catch(console.error);
+};
