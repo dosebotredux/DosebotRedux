3c342cf787299835b023f626c0e824167fe52ff9
Gabriel Morris <gabemorris@outlook.com>
2018-05-15 13:28:24 -0700

    hmm

--- commands/testembed.js
+++ commands/testembed.js
@@ -1,3 +1,5 @@
+const Discord = require("discord.js");
+
 exports.run = (client, message, args) => {
   const embed = new Discord.RichEmbed()
   .setTitle("This is your title, it can hold 256 characters")
