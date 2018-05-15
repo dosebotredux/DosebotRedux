d88eaf0c758db2df46cb457365a3988168dc8023
Gabriel Morris <gabemorris@outlook.com>
2018-05-15 13:40:52 -0700

    testing out functional embed

--- commands/dxmcalc.js
+++ commands/dxmcalc.js
@@ -1,3 +1,5 @@
+const Discord = require("discord.js");
+
 //calc dxm plateau dosages. usage --dxmcalc [weight in pounds]
 exports.run = (client, message, args) => {
   // message variables
@@ -21,7 +23,34 @@ exports.run = (client, message, args) => {
   var commonMaxStrongMin = Math.floor(400 * calculatedDoseModifier);
   var strongMaxHeavy = Math.floor(700 * calculatedDoseModifier);  
   var messages = [];
-  
+
+  const embed = new Discord.RichEmbed()
+  .setTitle("DXM Dosage Calculator")
+  .setAuthor("DoseBot", "http://www.dosebot.org/images/dose.png")
+  .setColor("ea125e")
+  // .setDescription("This is the main body of text, it can hold 2048 characters.")
+  .setFooter("Please use drugs responsiblty", "http://www.dosebot.org/images/dose.png")
+  .setImage("http://www.dosebot.org/images/dose.png")
+  .setThumbnail("http://www.dosebot.org/images/dose.png")
+  /*
+   * Takes a Date object, defaults to current date.
+   */
+  .setTimestamp()
+  .setURL("https://discord.js.org/#/docs/main/indev/class/RichEmbed")
+  .addField("First plateau",
+    lightMin + "-" + lightMaxCommonMin)
+  /*
+   * Inline fields may not display as inline if the thumbnail and/or image is too big.
+   */
+  .addField("Inline Field", "They can also be inline.", true)
+  /*
+   * Blank field, useful to create some space.
+   */
+  .addBlankField(true)
+  .addField("Inline Field 3", "You can have a maximum of 25 fields.", true);
+
+  message.channel.send({embed});
+
   // build message
   messages.push(`**DoseBot DXM calculator recommends:**`);
   messages.push(`\`\`\``);
