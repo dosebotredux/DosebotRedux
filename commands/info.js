119afa469998e097016cf97aa165da0877dd0d18
Gabriel Morris <gabemorris@outlook.com>
2018-05-15 14:35:41 -0700

    Formatting

--- commands/info.js
+++ commands/info.js
@@ -115,19 +115,20 @@ function buildDosageMessage(substance) {
       return undefined
     }
     
-    messages.push(`**${roa.name}`)
+    messages.push(`**${roa.name}**`)
     if (!!dose) {
       messages.push(`Threshold: ${dosageObjectToString(dose.threshold) || "no information"}`)
       messages.push(`Light: ${dosageObjectToString(dose.light) || "no information"}`)
       messages.push(`Common: ${dosageObjectToString(dose.common) || "no information"}`)
       messages.push(`Strong: ${dosageObjectToString(dose.strong) || "no information"}`)
       messages.push(`Heavy: ${dosageObjectToString(dose.heavy) || "no information"}`)
+      messages.push("")
     } else {
       messages.push("No dosage information.")
     }
     
     // Duration
-    messages.push(`${roa.name}`)
+    messages.push(`$**({roa.name})**`)
     if (!!roa.duration) {
       messages.push(`Onset: ${durationObjectToString(roa.duration.onset) || "no information"}`)
       messages.push(`Comeup: ${durationObjectToString(roa.duration.comeup) || "no information"}`)
@@ -135,6 +136,7 @@ function buildDosageMessage(substance) {
       messages.push(`Offset: ${durationObjectToString(roa.duration.offset) || "no information"}`)
       messages.push(`Afterglow: ${durationObjectToString(roa.duration.afterglow) || "no information"}`)
       messages.push(`Total: ${durationObjectToString(roa.duration.total) || "no information"}`)
+      messages.push("")
     } else {
       messages.push("```No duration information.```")
     }
