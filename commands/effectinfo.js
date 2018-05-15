571c8f5e6fda0ae3568b88b79e14af72d129c2d6
Gabriel Morris <gabemorris@outlook.com>
2018-05-15 13:21:35 -0700

    Refactor effectinfo with interpolation

--- commands/effectinfo.js
+++ commands/effectinfo.js
@@ -47,31 +47,64 @@ exports.run = (client, message, args) => {
   console.log(messageContent);
   
   // messageBuilder();
+  var messages = [];
+  
+  // build message
+  if (messageContent[3] !== undefined) {
+    messages.push(`**${messageContent[0]}**`);
+    messages.push(`\`\`\``);
+    messages.push(messageContent[1]);
+    messages.push(`\`\`\``);
+    messages.push(`More information: ${messageContent[2]}`);
+    messages.push(`Replication: ${messageContent[3]}`);
+  } else if (messageContent[0] !== undefined) {
+    messages.push(`**${messageContent[0]}**`);
+    messages.push(`\`\`\``);
+    messages.push(messageContent[1]);
+    messages.push(`\`\`\``);
+    messages.push(`More information: ${messageContent[2]}`);
+  } else {
+    messages.push(`**Error**: Undefined effect`);
+  }
+
+  message.channel.send(messages.join("\n")).catch(console.error);
+  
+  // messages.push(`**DoseBot DXM calculator recommends:**`);
+  // messages.push(`\`\`\``);
+  // messages.push(`1st plateau: ${lightMin} - ${lightMaxCommonMin}mg`);
+  // messages.push(`2nd plateau: ${lightMaxCommonMin} - ${commonMaxStrongMin}mg`);
+  // messages.push(`3rd plateau: ${commonMaxStrongMin} - ${strongMaxHeavy}mg`);
+  // messages.push(`4th plateau: ${strongMaxHeavy}mg+\n`);
+  // messages.push(`\`\`\``)
+  // messages.push("**Warning:** These recommendations are an approximation, please take into account your own personal tolerance and start with lower dosages. Doses exceeding 1500mg are potentially fatal.");
+  
+  // // join message with new lines
+  // message.channel.send(messages.join("\n")).catch(console.error);
   
   // spaghetti message builder
-  if (messageContent[3] !== undefined) {
-    message.channel
-    .send("**[:exclamation:] " + messageContent[0] + "**" +
-    "\n" +
-    "```" + "\n" + messageContent[1] + "\n" + "```" +
-    "\n" +
-    "**More information:** " + messageContent[2] +
-    "\n\n" +
-    "**Replication:** " + messageContent[3]
-  )
-  .catch(console.error);
-} else if (messageContent[0] !== undefined) {
-  message.channel
-  .send("**" + messageContent[0] + "**" +
-  "\n" +
-  "```" + messageContent[1] + "```" +
-  "\n" +
-  "**More information:** " + messageContent[2])
-} else {
-  message.channel
-  .send("**Error**: Undefined effect")
-  .catch(console.error);
-}
+//   if (messageContent[3] !== undefined) {
+//     message.channel
+//     .send("**[:exclamation:] " + messageContent[0] + "**" +
+//     "\n" +
+//     "```" + "\n" + messageContent[1] + "\n" + "```" +
+//     "\n" +
+//     "**More information:** " + messageContent[2] +
+//     "\n\n" +
+//     "**Replication:** " + messageContent[3]
+//   )
+//   .catch(console.error);
+// } else if (messageContent[0] !== undefined) {
+//   message.channel
+//   .send("**" + messageContent[0] + "**" +
+//   "\n" +
+//   "```" + messageContent[1] + "```" +
+//   "\n" +
+//   "**More information:** " + messageContent[2])
+// } else {
+//   message.channel
+//   .send("**Error**: Undefined effect")
+//   .catch(console.error);
+// }
 };
 
 // // functions
