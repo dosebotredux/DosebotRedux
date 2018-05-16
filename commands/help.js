6a1affcd20648c54304094dabe8174beb743850f
Gabriel Morris <gabemorris@outlook.com>
2018-05-16 09:37:01 -0700

    Port help to embed

--- commands/help.js
+++ commands/help.js
@@ -1,8 +1,33 @@
+const Discord = require("discord.js");
+
 //displays list of commands. in future should scan commands directory and display info for each programmatically/dynamically
 exports.run = (client, message, args) => {
-  message.channel
-  .send(
-    "```Available commands: \n --about\n --help \n --psytolerance [number of days since last dose] \n --info [substance]\n --effects [substance]\n --effectinfo [effect] \n --randomeffect \n --dxmcalc [weight in pounds] \n --combochart```"
-  )
-  .catch(console.error);
+  const embed = new Discord.RichEmbed()
+    .setTitle("DoseBot Help")
+    .setAuthor("DoseBot", "http://www.dosebot.org/images/dose.png")
+    .setColor("747474")
+    .setFooter("Please use drugs responsibly", "http://www.dosebot.org/images/dose.png")
+    .setThumbnail("https://kek.gg/i/svRNH.png")
+    .setTimestamp()
+    .setURL("http://www.dosebot.org")
+    .addField("Available commands", buildCommandList())
 };
+
+function buildCommandList() {
+  var commands = [
+    "--about",
+    "--badtrip",
+    "--bobross",
+    "--combochart",
+    "--dxmcalc [weight in lbs]",
+    "--effectinfo [effect]",
+    "--effects [substance]",
+    "--info [substance]",
+    "--memes",
+    "--psytolerance [days]",
+    "--randomeffect",
+    "--triptoy"
+  ];
+
+  return commands.join("\n");
+}
