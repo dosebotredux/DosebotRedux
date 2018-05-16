f0a32476a890d81f4b99c897268c47623fa5e309
Gabriel Morris <gabemorris@outlook.com>
2018-05-16 09:41:15 -0700

    remove old about

--- commands/about.js
+++ commands/about.js
@@ -1,3 +1,5 @@
+const Discord = require("discord.js");
+
 //about message
 exports.run = (client, message, args) => {
   const embed = new Discord.RichEmbed()
@@ -11,10 +13,4 @@ exports.run = (client, message, args) => {
   .addField("Available commands", "DoseBot automatically sources dosage, duration, tolerance, and harm reduction information from <http://www.psychonautwiki.org> and <https://effectindex.com>.\n\nIt was created by <@278301453620084736>, <@295422447887450114>, <@249800037050220545>, <@183993728178978818>, and <@247965158364676104> with the goal of raising awareness of harm reduction best practices, as well as the Subjective Effect Index. Based on KGB by Stas Constantine\n\nFor a complete list of commands type '--help'.\n\nFor more information see <https://effectindex.com/dosebot> or join the Subjective Effect Documentation discord server at <https://discord.gg/2J5XBfX>.")
 
   message.channel.send({embed});
-
-  message.channel
-  .send(
-    "DoseBot - <http://www.dosebot.org>\n\nDoseBot automatically sources dosage, duration, tolerance, and harm reduction information from <http://www.psychonautwiki.org> and <https://effectindex.com>.\n\nIt was created by <@278301453620084736>, <@295422447887450114>, <@249800037050220545>, <@183993728178978818>, and <@247965158364676104> with the goal of raising awareness of harm reduction best practices, as well as the Subjective Effect Index. Based on KGB by Stas Constantine\n\nFor a complete list of commands type '--help'.\n\nFor more information see <https://effectindex.com/dosebot> or join the Subjective Effect Documentation discord server at <https://discord.gg/2J5XBfX>."
-  )
-  .catch(console.error);
 };
