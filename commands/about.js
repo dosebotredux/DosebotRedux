2f811d41477436add2fa7f3b63fda07234d2fdc8
Gabriel Morris <gabemorris@outlook.com>
2018-05-16 09:47:11 -0700

    Oops wrong function

--- commands/about.js
+++ commands/about.js
@@ -8,9 +8,8 @@ exports.run = (client, message, args) => {
   .setFooter("Please use drugs responsibly", "https://kek.gg/i/JGVVV.png")
   .setThumbnail("https://kek.gg/i/svRNH.png")
   .setTimestamp()
-  .setImage("https://psychonautwiki.org/w/thumb.php,qf=You_do_not_need_to_understand.gif,awidth=419.pagespeed.ce._yCpj9CanE.gif")
   .setURL("http://www.dosebot.org")
-  .addField("Assistance", "Taken drugs or have questions about taking drugs? Concerned or have any questions? Visit the TripSit Chat: <https://chat.tripsit.me/>\nNeed a healthy dose of **good vibes**? Check out the PsychonautWiki Good Vibes page: <https://psychonautwiki.org/wiki/Good_vibe>\nThis Youtube channel is dedicated to sending positive vibes: <https://www.youtube.com/user/TheMeditativeMind>\n**Everything is going to be okay!** You are awesome, everyone here loves, cares, and supports you! **Take long, slow, deep breaths** and focus on each breath in and out!")
+  .addField("Assistance", "DoseBot automatically sources dosage, duration, tolerance, and harm reduction information from <http://www.psychonautwiki.org> and <https://effectindex.com>.\n\nIt was created by <@278301453620084736>, <@295422447887450114>, <@249800037050220545>, <@183993728178978818>, and <@247965158364676104> with the goal of raising awareness of harm reduction best practices, as well as the Subjective Effect Index. Based on KGB by Stas Constantine\n\nFor a complete list of commands type '--help'.\n\nFor more information see <https://effectindex.com/dosebot> or join the Subjective Effect Documentation discord server at <https://discord.gg/2J5XBfX>.")
 
   message.channel.send({embed});
 };
--- commands/badtrip.js
+++ commands/badtrip.js
@@ -1,8 +1,17 @@
-//Kaylee message
+const Discord = require("discord.js");
+
+//Badtrip command
 exports.run = (client, message, args) => {
-  message.channel
-  .send(
-      "**Assistance**: Taken drugs or have questions about taking drugs? Concerned or have any questions? Visit the TripSit Chat: <https://chat.tripsit.me/>\n\nNeed a healthy dose of **good vibes?** Check out the PsychonautWiki Good Vibes page: <https://psychonautwiki.org/wiki/Good_vibes>\n\nThis Youtube channel is dedicated to sending positive vibes: <https://www.youtube.com/user/TheMeditativeMind>\n\n**Everything is going to be okay!** You are *awesome*, everyone here loves, cares, and supports you! **Take long, slow, deep breaths** and focus on each breath in and out!"
-  )
+  const embed = new Discord.RichEmbed()
+  .setAuthor("DoseBot", "https://kek.gg/i/JGVVV.png")
+  .setColor("747474")
+  .setFooter("Please use drugs responsibly", "https://kek.gg/i/JGVVV.png")
+  .setThumbnail("https://kek.gg/i/svRNH.png")
+  .setTimestamp()
+  .setImage("https://psychonautwiki.org/w/thumb.php,qf=You_do_not_need_to_understand.gif,awidth=419.pagespeed.ce._yCpj9CanE.gif")
+  .setURL("http://www.dosebot.org")
+  .addField("Assistance", "Taken drugs or have questions about taking drugs? Concerned or have any questions? Visit the TripSit Chat: <https://chat.tripsit.me/>\n\nNeed a healthy dose of **good vibes**? Check out the PsychonautWiki Good Vibes page: <https://psychonautwiki.org/wiki/Good_vibe>\n\nThis Youtube channel is dedicated to sending positive vibes: <https://www.youtube.com/user/TheMeditativeMind>\n\n**Everything is going to be okay!** You are awesome, everyone here loves, cares, and supports you! **Take long, slow, deep breaths** and focus on each breath in and out!")
+
+  message.channel.send({embed})
   .catch(console.error);
 };
