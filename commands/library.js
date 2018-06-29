8d396c5e113193ed842ab208694575dda9599fda
Gabriel Morris <gabemorris@outlook.com>
2018-06-29 13:37:06 -0700

    SCOPE

--- commands/library.js
+++ commands/library.js
@@ -8,44 +8,44 @@ exports.run = (client, message, args) => {
   .toLowerCase()
   .replace("--library ", "", -1)
   .replace(/-/g, "", -1)
-
+  
   console.log("Search: " + search);
-
+  
   var testArr = [];
-
+  
   for (let i = 0; i < library.library.length; i++) {
     if (library.library[i].toLowerCase().includes(search.toLowerCase())) {
       var strForwardSlash = library.library[i].replace(/\\/g, "/");
       var strSpaces = strForwardSlash.replace(/ /g, "%20");
-
+      
       testArr.push(strSpaces);
     }
   }
-
+  
   const theEye = "http://the-eye.eu/public/Psychedelics/Psychedelic%20Praxis%20Library%203.0";
-
+  
   for (let i = 0; i < testArr.length; i++) {
     testArr[i] = theEye + testArr[i];
   }
-
+  
   console.log(testArr);
   
   message.channel
-  .send(buildMessage(search))
+  .send(buildMessage())
   .catch(console.error);
-};
-
-function buildMessage(str) {
-  var results = [`Search results for: ${str}`];
-
-  if (testArr.length > 5) {
-    for (let i = 0; i < 5; i++) {
-      results.push(testArr[i]);
+  
+  function buildMessage() {
+    var results = [`Search results for: ${search}`];
+    
+    if (testArr.length > 5) {
+      for (let i = 0; i < 5; i++) {
+        results.push(testArr[i]);
+      }
+    } else {
+      for (let i = 0; i < testArr.length; i++) {
+        results.push(testArr[i]);
+      }
     }
-  } else {
-    for (let i = 0; i < testArr.length; i++) {
-      results.push(testArr[i]);
-    }
+    return results.join("\n\n");
   }
-  return results.join("\n\n");
-}
+};
