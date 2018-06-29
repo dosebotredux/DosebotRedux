9cc70e1023e0343be30bb20dda5a6b95c6ee6e6a
Gabriel Morris <gabemorris@outlook.com>
2018-06-29 13:01:41 -0700

    Testing parsing URL

--- commands/library.js
+++ commands/library.js
@@ -17,14 +17,27 @@ exports.run = (client, message, args) => {
   for (let i = 0; i < library.library.length; i++) {
     if (library.library[i].toLowerCase().includes(search.toLowerCase())) {
       testArr.push(library.library[i]);
+
+      var strForwardSlash = library.library[i].replace(/\\\\/g, "/");
+      var strSpaces = strForwardSlash.replace(/ /g, "%20");
+
+      testArr.push(strSpaces);
     }
   }
 
   console.log(testArr);
   
   // message.channel
-  // .send(
-  //   "ALL HAIL THE GLORIOUS CATGIRL, <@183993728178978818>\nKAYLEE APPRECIATION DAY\nKAYLEE APPRECIATION DAY\nKAYLEE APPRECIATION DAY\nKAYLEE APPRECIATION DAY\n"
-  // )
+  // .send()
   // .catch(console.error);
 };
+
+// function parseURL(str) {
+//   const theEye = "http://the-eye.eu/public/Psychedelics/Psychedelic%20Praxis%20Library%203.0";
+//   //returns
+//   // \\Collections by Author\\Alan Watts\\1960 - Alan Watts - The New Alchemy.pdf
+
+// }
+
+//http://the-eye.eu/public/Psychedelics/Psychedelic%20Praxis%20Library%203.0/
+//https://the-eye.eu/public/Psychedelics/texts%20by%20author/Alan%20Watts/1960%20-%20Alan%20Watts%20-%20The%20New%20Alchemy.pdf
