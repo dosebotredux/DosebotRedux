2d516f32a108194bc9c03dbfba18dcd607df660f
Gabriel Morris <gabemorris@outlook.com>
2018-06-29 13:06:24 -0700

    Weird regex stuff

--- commands/library.js
+++ commands/library.js
@@ -18,14 +18,16 @@ exports.run = (client, message, args) => {
     if (library.library[i].toLowerCase().includes(search.toLowerCase())) {
       testArr.push(library.library[i]);
 
-      var strForwardSlash = library.library[i].replace(/\\\\/g, "/");
+      var strForwardSlash = library.library[i].replace(/\/\//g, "/");
+      console.log("forward slash: " + strForwardSlash);
       var strSpaces = strForwardSlash.replace(/ /g, "%20");
+      console.log("spaces: " + strSpaces);
 
       testArr.push(strSpaces);
     }
   }
 
-  console.log(testArr);
+  // console.log(testArr);
   
   // message.channel
   // .send()
