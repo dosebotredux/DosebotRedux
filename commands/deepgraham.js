97563be3725b8c0ded1b1c8223eb8c418faebd44
Gabriel Morris <gabemorris@outlook.com>
2018-07-04 15:20:17 -0700

    deep graham

---
+++ commands/deepgraham.js
@@ -0,0 +1,8 @@
+// Graham message
+exports.run = (client, message, args) => {
+  message.channel
+  .send(
+    "<@200986075705901056> https://kek.gg/i/4XYhFB.jpg"
+  )
+  .catch(console.error);
+};
