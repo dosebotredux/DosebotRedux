2eb6ad687e0de710191874e8fd92d999ebb861d0
Gabriel Morris <gabemorris@outlook.com>
2018-07-03 21:57:08 -0700

    Fixed zen

--- commands/zen.js
+++ commands/zen.js
@@ -2,7 +2,7 @@
 exports.run = (client, message, args) => {
   message.channel
   .send(
-    "<@200986075705901056> \"Wyoming is flat\""
+    "<@254029997323452416> \"Wyoming is flat\""
   )
   .catch(console.error);
 };
