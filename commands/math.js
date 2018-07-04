9847a57bbd8eb9da44947655cca96f8d731ab295
Gabriel Morris <gabemorris@outlook.com>
2018-07-03 22:00:33 -0700

    Math meme

---
+++ commands/math.js
@@ -0,0 +1,8 @@
+// Josildo 2 message
+exports.run = (client, message, args) => {
+  message.channel
+  .send(
+    "<@295422447887450114> https://kek.gg/i/3FTSjG.png"
+  )
+  .catch(console.error);
+};
