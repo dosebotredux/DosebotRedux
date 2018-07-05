e92feb02cd0748d56118788bd32ada32ed5fc020
Gabriel Morris <gabemorris@outlook.com>
2018-07-05 13:19:26 -0700

    Remove stale meme and update memelist

--- commands/eve.js
+++
@@ -1,8 +0,0 @@
-//EVE message
-exports.run = (client, message, args) => {
-  message.channel
-  .send(
-    "Remember to drink water <@301136316635152385>\nEVE APPRECIATION DAY\nEVE APPRECIATION DAY\nEVE APPRECIATION DAY\nEVE APPRECIATION DAY\n"
-  )
-  .catch(console.error);
-};
