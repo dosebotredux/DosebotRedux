a7b9c9f1780c25b7377e49003d14cf10afaf3ea2
Gabriel Morris <gabemorris@outlook.com>
2018-07-06 20:10:07 -0700

    apple update

---
+++ commands/apple.js
@@ -0,0 +1,8 @@
+//Josie message
+exports.run = (client, message, args) => {
+  message.channel
+  .send(
+    "HAPPY BIRTHDAY TO THE GLORIOUS ADULT TWINK, <@198667544930811905>\nAPPLE APPRECIATION DAY\nAPPLE APPRECIATION DAY\nAPPLE APPRECIATION DAY\nAPPLE APPRECIATION DAY\n"
+  )
+  .catch(console.error);
+};
