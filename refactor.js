af9eff3a8347640743cfb0281762f15ab75a30fd
Gabriel Morris <gabemorris@outlook.com>
2018-07-12 10:21:09 -0700

    Abstraction in refactor

--- refactor.js
+++ refactor.js
@@ -9,8 +9,33 @@ var util = require("util");
 
 // On ready logic
 client.on("ready", () => {
+  // Update game message on launch
+  updateGameMessage();
+  console.log("DoseBot is online - beep boop");
+});
+
+// Log new guild joins
+client.on("guildCreate", guild => {
+  // Update game message on new server join
+  updateGameMessage();
+  console.log(`New server joined - Name: ${guild.name} Members: ${guild.memberCount}`)
+});
+
+// Pass messages to the CommandSystem
+client.on("message", message => {
+  CommandSystem.execute(client, message)
+});
+
+//
+CommandSystem.load(function() {
+  console.log("Command system loaded.")
+});
+
+client.login(token);
+
+function updateGameMessage() {
   // Logs guilds and member counts 
-  const servers = client.guilds;
+  let servers = client.guilds;
   let userCount = 0;
   let servercount = 0;
   
@@ -28,22 +53,4 @@ client.on("ready", () => {
   console.log(`Currently serving ${userCount} users on ${servercount} servers`);
   let botGame = `${userCount} users on ${servercount} servers`;
   client.user.setActivity(botGame).then(presence => console.log(`Game set to ${botGame}`)).catch(console.error);
-  console.log("DoseBot is online - beep boop");
-});
-
-// Log new guild joins
-client.on("guildCreate", guild => {
-  console.log(`New server joined - Name: ${guild.name} Members: ${guild.memberCount}`)
-});
-
-// Pass messages to the CommandSystem
-client.on("message", message => {
-  CommandSystem.execute(client, message)
-});
-
-//
-CommandSystem.load(function() {
-  console.log("Command system loaded.")
-});
-
-client.login(token);
+}
