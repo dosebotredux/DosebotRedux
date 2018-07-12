a2ba56cc7748882e65585740c4b80ece42ef8428
Gabriel Morris <gabemorris@outlook.com>
2018-07-12 10:00:17 -0700

    Select random effects

--- commands/effects.js
+++ commands/effects.js
@@ -3,57 +3,82 @@ const sanitizeSubstanceName = require("../include/sanitize-substance-name.js")
 
 exports.run = (client, message, args) => {
   const { request } = require("graphql-request");
-
+  
   var str = message.content;
   var result = str.split(" ");
   var drug = str
-    .toLowerCase()
-    .replace("--effects ", "", -1)
-    .replace(/-/g, "", -1)
-    .replace(/ /g, "", -1); //removes all symbols and puts everything in lower case so bot finds the images easier
+  .toLowerCase()
+  .replace("--effects ", "", -1)
+  .replace(/-/g, "", -1)
+  .replace(/ /g, "", -1); //removes all symbols and puts everything in lower case so bot finds the images easier
   drug = sanitizeSubstanceName(drug)
-
+  
   // loads graphql query from separate file as "query" variable
   const query = require("../queries/effects.js").effect(drug)
-
+  
   request("https://api.psychonautwiki.org", query).then(data => {
-    console.log(data) // SHOW ME WHAT YOU GOT
+  console.log(data) // SHOW ME WHAT YOU GOT
+  
+  if (data.substances.length == 0) {
+    message.channel.send(`There are no substances matching \`${drug}\` on PsychonautWiki.`).catch(console.error)
+    return
+  }
+  
+  if (data.substances.length > 1) {
+    message.channel.send(`There are multiple substances matching \`${drug}\` on PsychonautWiki.`).catch(console.error)
+    return
+  }
+  
+  // const substance = data.substances[0]
+  // const effects = substance.effects
+  
+  // var messages = []
+  
+  // // loops through effects and add their name to the message variable
+  // for (let i = 0; i < 10; i++) {
+  //   messages.push(`-${effects[i].name}`)
+  // }
+  
+  // let effectsJoined = messages.join("\n")
+  
+  const embed = new Discord.RichEmbed()
+  .setTitle(`${substance.name} effect information`)
+  .setAuthor("DoseBot", "https://kek.gg/i/JGVVV.png")
+  .setColor("747474")
+  .setFooter("Please use drugs responsibly", "https://kek.gg/i/JGVVV.png")
+  .setThumbnail("https://kek.gg/i/svRNH.png")
+  .setTimestamp()
+  .setURL("http://www.dosebot.org")
+  .addField(`Effects`, effectsJoined)
+  .addField(`More information`, createFullEffectListLink(substance))
+  
+  message.channel.send({embed}).catch(console.error);
+}).catch(console.log)
+}
 
-    if (data.substances.length == 0) {
-      message.channel.send(`There are no substances matching \`${drug}\` on PsychonautWiki.`).catch(console.error)
-      return
-    }
+function createEffectsList(substance) {
+  const substance = data.substances[0];
+  const effects = substance.effects;
+  const numberOfEffects = effects.length;
+  const randomNumberArray = [];
+  
+  while (randomNumberArray.length < 10) {
+    randomNumberArray.push(Math.floor(Math.random() * numberOfEffects));
+  }
 
-    if (data.substances.length > 1) {
-      message.channel.send(`There are multiple substances matching \`${drug}\` on PsychonautWiki.`).catch(console.error)
-      return
-    }
+  var messages = [];
 
-    const substance = data.substances[0]
-    const effects = substance.effects
-
-    var messages = []
-
-    // loops through effects and add their name to the message variable
-    for (let i = 0; i < 10; i++) {
-      messages.push(`-${effects[i].name}`)
-    }
-
-    let effectsJoined = messages.join("\n")
-
-    const embed = new Discord.RichEmbed()
-    .setTitle(`${substance.name} effect information`)
-    .setAuthor("DoseBot", "https://kek.gg/i/JGVVV.png")
-    .setColor("747474")
-    .setFooter("Please use drugs responsibly", "https://kek.gg/i/JGVVV.png")
-    .setThumbnail("https://kek.gg/i/svRNH.png")
-    .setTimestamp()
-    .setURL("http://www.dosebot.org")
-    .addField(`Effects`, effectsJoined)
-    .addField(`More information`, createFullEffectListLink(substance))
-
-    message.channel.send({embed}).catch(console.error);
-  }).catch(console.log)
+  randomNumberArray.forEach(number, index => {
+    messages.push(`-${effects[number[index]].name}`);
+  });
+  
+  
+  // // loops through effects and add their name to the message variable
+  // for (let i = 0; i < 10; i++) {
+  //   messages.push(`-${effects[i].name}`)
+  // }
+  
+  return messages.join("\n");
 }
 
 function createFullEffectListLink(substance) {
