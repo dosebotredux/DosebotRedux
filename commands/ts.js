1ffbcd24c8a74c85ccc86a68349fff36dac294c4
Gabriel Morris <gabemorris@outlook.com>
2018-07-16 22:09:05 -0700

    formatting

--- commands/ts.js
+++ commands/ts.js
@@ -76,9 +76,7 @@ exports.run = (client, message, args) => {
     function buildTSLinksField(substance) {
       return `[PsychonautWiki](https://psychonautwiki.org/wiki/${
         substance.name
-      })\n[Effect Index](https://beta.effectindex.com)\n[Drug combination chart](https://wiki.tripsit.me/images/3/3a/Combo_2.png)\n[TripSit](${
-        substance.name
-      })\nInformation sourced from TripSit`;
+      })\n[Effect Index](https://beta.effectindex.com)\n[Drug combination chart](https://wiki.tripsit.me/images/3/3a/Combo_2.png)\n[TripSit](https://www.tripsit.org)\n\nInformation sourced from TripSit`;
     }
   }
 };
