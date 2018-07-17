e0ce5013c935fa439d70132e74d9c6c3ddd54d1b
Gabriel Morris <gabemorris@outlook.com>
2018-07-16 21:23:50 -0700

    log summary

--- commands/ts.js
+++ commands/ts.js
@@ -5,13 +5,24 @@ const request = require("request");
 // Mascot lol
 exports.run = (client, message, args) => {
   console.log(`**********Executing TS on ${message.guild.name}**********`);
+  // Capture messages posted to a given channel and remove all symbols and put everything into lower case
+  var str = message.content;
+  var result = str.split(" ");
+  var drug = str
+    .toLowerCase()
+    .replace("--ts ", "", -1)
+    .replace(/-/g, "", -1)
+    .replace(/ /g, "", -1);
 
-  let tripSitURL = "http://tripbot.tripsit.me/api/tripsit/getDrug?name=lsd";
+  // Sanitizes input names to match PsychonautWiki API names
+  drug = sanitizeSubstanceName(drug);
+
+  let tripSitURL = `http://tripbot.tripsit.me/api/tripsit/getDrug?name=${drug}`;
   let queryResults;
   request(tripSitURL, function(error, response, body) {
     queryResults = JSON.parse(body);
-    console.log(queryResults);
+    console.log(queryResults.data[0].summary);
   });
 
-  // message.channel.send(response).catch(console.error);
+  message.channel.send(queryResults.data[0].summary).catch(console.error);
 };
