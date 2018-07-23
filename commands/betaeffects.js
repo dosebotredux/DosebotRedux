27cb2901eca6e8d8077e6b52844a5cef467a6b87
Gabriel Morris <gabemorris@outlook.com>
2018-07-22 20:03:12 -0700

    test for EI effects

---
+++ commands/betaeffects.js
@@ -0,0 +1,48 @@
+// About message
+const Discord = require("discord.js");
+const rp = require("request-promise");
+
+exports.run = (client, message, args) => {
+  console.log(
+    `**********Executing betaeffects on ${message.guild.name}**********`
+  );
+
+  // Capture messages posted to a given channel and remove all symbols and put everything into lower case
+  var str = message.content; // --betaeffects acuity enhancement
+  var result = str.split(" "); // [--betaeffects, acuity, enhancement]
+  var effect = str
+    .toLowerCase()
+    .replace("--betaeffects ", "", -1)
+    .replace(/-/g, "", -1)
+    .replace(/ /g, "-", -1); // acuity enhancement
+
+  console(`effect: ${effect}`);
+
+  // Declare the location of the API URL
+  let url = `https://beta.effectindex.com/api/effects/${effect}`;
+
+  rp(`${url}`)
+    .then(function(body) {
+      console.log(body);
+    })
+    .catch(function(err) {
+      console.error(err);
+    });
+
+  const embed = new Discord.RichEmbed()
+    .setAuthor("DoseBot", "https://kek.gg/i/JGVVV.png")
+    .setColor("747474")
+    .setFooter("Please use drugs responsibly", "https://kek.gg/i/JGVVV.png")
+    .setThumbnail("https://kek.gg/i/svRNH.png")
+    .setTimestamp()
+    .setURL("http://www.dosebot.org")
+    .addField("Title", "Summary field");
+
+  message.channel.send({ embed });
+
+  function createSummaryField() {}
+
+  function createLinksField() {}
+
+  function createReplicationField() {}
+};
