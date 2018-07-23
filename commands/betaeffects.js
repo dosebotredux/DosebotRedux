553364f55148f2fe96cd5804c160418cd64fdfdc
Gabriel Morris <gabemorris@outlook.com>
2018-07-22 21:00:19 -0700

    typo

--- commands/betaeffects.js
+++ commands/betaeffects.js
@@ -67,7 +67,7 @@ exports.run = (client, message, args) => {
   }
 
   function createReplicationField(effectJSON) {
-    if (effectJSON.replicaitons.length > 1) {
+    if (effectJSON.effect.replications.length > 1) {
       const replicationName = effectJSON.effect[0].resource;
       const replicationURL = `https://beta.effectindex.com/img/gallery/${replicationName}`;
 
