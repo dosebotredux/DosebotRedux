2d9ec5a32508b50210bd62e800121b9af99a8d97
Gabriel Morris <gabemorris@outlook.com>
2018-07-27 00:58:18 -0700

    Function abstraction

--- commands/about.js
+++ commands/about.js
@@ -4,7 +4,10 @@ const Discord = require("discord.js");
 exports.run = (client, message, args) => {
   console.log(`**********Executing about on ${message.guild.name}**********`);
 
-  const embed = new Discord.RichEmbed()
+  message.channel.send({ generateAboutMessage() });
+
+  function generateAboutMessage() {
+    const embed = new Discord.RichEmbed()
     .setAuthor("DoseBot", "https://i.imgur.com/7R8WDwE.png")
     .setColor("747474")
     .setFooter(
@@ -19,5 +22,6 @@ exports.run = (client, message, args) => {
       "DoseBot automatically sources dosage, duration, tolerance, and harm reduction information from [PsychonautWiki](http://www.psychonautwiki.org) and [Effect Index](https://effectindex.com).\n\nIt was created by <@278301453620084736>, <@295422447887450114>, <@249800037050220545>, <@183993728178978818>, and <@247965158364676104> with the goal of raising awareness of harm reduction best practices, as well as the Subjective Effect Index. Based on KGB by Stas Constantine\n\nFor a complete list of commands type '--help'.\n\nFor more information see [DoseBot on Effect Index](https://effectindex.com/dosebot) or join the Subjective Effect Documentation [Discord server](https://discord.gg/2J5XBfX)."
     );
 
-  message.channel.send({ embed });
+    return embed;
+  }
 };
