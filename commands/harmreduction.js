db2591af3033fd115c81edd75f77e7711fe975b1
Gabriel <gabemorris@outlook.com>
2018-08-13 18:43:47 -0700

    Harm reduction

---
+++ commands/harmreduction.js
@@ -0,0 +1,10 @@
+// harm reduction message
+exports.run = (client, message, args) => {
+  console.log(
+    `**********Executing harm reduction on ${message.guild.name}**********`
+  );
+
+  message.channel
+    .send("Are you practicing H A R M  R E D U C T I O N???")
+    .catch(console.error);
+};
