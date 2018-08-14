b249dec5f3f1892d9daff28f6927bee0ba35c588
Gabriel <gabemorris@outlook.com>
2018-08-13 19:19:01 -0700

    image for hr

--- commands/harmreduction.js
+++ commands/harmreduction.js
@@ -5,6 +5,8 @@ exports.run = (client, message, args) => {
   );
 
   message.channel
-    .send("Are you practicing H A R M  R E D U C T I O N???")
+    .send(
+      "Are you practicing H A R M  R E D U C T I O N???\n\nhttps://i.imgur.com/nIpaz1y.png"
+    )
     .catch(console.error);
 };
