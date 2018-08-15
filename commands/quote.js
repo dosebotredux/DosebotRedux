46acb0d5bb0914c1ed59601eaab847c64f99f0c0
Gabriel <gabemorris@outlook.com>
2018-08-14 20:52:07 -0700

    hmm

--- commands/quote.js
+++ commands/quote.js
@@ -4,6 +4,9 @@ const quotes = require("../include/quotes.json");
 exports.run = (client, message, args) => {
   console.log(`**********Executing quote on ${message.guild.name}**********`);
   let random = Math.random() * quotes.quotes.length;
+  console.log(quotes);
+  console.log(quotes.quotes[random]);
+  console.log(random);
 
   message.channel.send(quotes.quotes[random]).catch(console.error);
 };
