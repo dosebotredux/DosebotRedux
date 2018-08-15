59dd20d02912556e284fe0081fbc24f66e526b15
Gabriel Morris <gabemorris@outlook.com>
2018-08-15 11:17:21 -0700

    hmm

--- commands/addquote.js
+++ commands/addquote.js
@@ -3,30 +3,22 @@ const mongoose = require("mongoose");
 exports.run = (client, message, args) => {
   console.log(`**********Executing heart on ${message.guild.name}**********`);
 
-  mongoose.connect(
-    `mongodb://${process.env.MONGO_DB_USER}:${
-      process.env.MONGO_DB_PASS
-    }@ds121282.mlab.com:21282/dosebot_quotes`
-  );
-
-  const db = mongoose.connection;
-
   db.on("error", console.error.bind(console, "connection error:"));
-  db.once("open", function() {
-    console.log(`Connected to database`);
-    let quoteSchema = new mongoose.Schema({
-      quote: String,
-      author: String
-    });
-    let Quote = mongoose.model("quotes", quoteSchema);
+  // db.once("open", function() {
+  //   console.log(`Connected to database`);
+  //   let quoteSchema = new mongoose.Schema({
+  //     quote: String,
+  //     author: String
+  //   });
+  //   let Quote = mongoose.model("quotes", quoteSchema);
 
-    let quoteToAdd = new Quote({
-      quote: "Test",
-      author: "Bob"
-    });
+  //   let quoteToAdd = new Quote({
+  //     quote: "Test",
+  //     author: "Bob"
+  //   });
 
-    quoteToAdd.save(function(err, quoteToAdd) {
-      if (err) return console.error(err);
-    });
-  });
+  //   quoteToAdd.save(function(err, quoteToAdd) {
+  //     if (err) return console.error(err);
+  //   });
+  // });
 };
