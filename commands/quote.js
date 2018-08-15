0ef023636784907c658b6fc432898ee0cadaba3f
Gabriel <gabemorris@outlook.com>
2018-08-14 21:57:52 -0700

    test custom names

--- commands/quote.js
+++ commands/quote.js
@@ -4,11 +4,23 @@ const Discord = require("discord.js");
 // quote message
 exports.run = (client, message, args) => {
   console.log(`**********Executing quote on ${message.guild.name}**********`);
+  let msgString = message.content
+    .replace(`--quote`, ``, -1)
+    .replace(/-/g, ``, -1);
+  let msgArr = msgString.split(` `);
+  let name;
+
   let randomQuoteNumber = Math.floor(Math.random() * quotes.quotes.length);
   let quote = quotes.quotes[randomQuoteNumber];
 
   let randomUserNumber = Math.floor(Math.random() * quotes.names.length);
-  let name = quotes.names[randomUserNumber];
+
+  if (msgArr.length < 1) {
+    name = quotes.names[randomUserNumber];
+  } else {
+    // [name, name, name]
+    name = msgArr.splice(0, msgArr.length).join(` `);
+  }
 
   const embed = new Discord.RichEmbed()
     .setAuthor("DoseBot", "https://i.imgur.com/7R8WDwE.png")
