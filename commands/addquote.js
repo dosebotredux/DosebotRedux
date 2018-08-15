8ae6ad4da73363376cd2bceddb503268bcd1b33b
Gabriel Morris <gabemorris@outlook.com>
2018-08-15 11:27:29 -0700

    try using regular driver

--- commands/addquote.js
+++ commands/addquote.js
@@ -1,34 +1,20 @@
-const mongoose = require("mongoose");
 // add quote
 exports.run = (client, message, args) => {
   console.log(`**********Executing heart on ${message.guild.name}**********`);
 
-  mongoose.connect(
-    `mongodb://${process.env.MONGO_DB_USER}:${
-      process.env.MONGO_DB_PASS
-    }@ds121282.mlab.com:21282/dosebot_quotes`
+  const MongoClient = require("mongodb").MongoClient;
+  const url = `mongodb://${process.env.MONGO_DB_USER}:${
+    process.env.MONGO_DB_PASS
+  }@ds121282.mlab.com:21282/dosebot_quotes`;
+  const dbName = "dosebot_quotes";
+
+  MongoClient.connect(
+    url,
+    function(err, client) {
+      console.log(`Connected to Mongo`);
+      const db = client.db(dbName);
+
+      client.close();
+    }
   );
-
-  const db = mongoose.connection;
-
-  let quoteSchema = new mongoose.Schema({
-    quote: String,
-    author: String
-  });
-
-  db.on("error", console.error.bind(console, "connection error:"));
-  db.once("open", function() {
-    console.log(`Connected to database`);
-
-    let Quote = mongoose.model("quotes", quoteSchema);
-
-    let quoteToAdd = new Quote({
-      quote: "Test",
-      author: "Bob"
-    });
-
-    quoteToAdd.save(function(err, quoteToAdd) {
-      if (err) return console.error(err);
-    });
-  });
 };
