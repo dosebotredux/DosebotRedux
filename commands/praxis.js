6ab1b7d0eae928efa2774a8e2129122f710b3ed0
Gabriel <gabemorris@outlook.com>
2018-08-14 18:08:15 -0700

    working?

--- commands/praxis.js
+++ commands/praxis.js
@@ -1,19 +1,25 @@
 // praxis message
 exports.run = (client, message, args) => {
   console.log(`**********Executing praxis**********`);
-  // message.channel.send();
   //350132819307003905 praxis
   //332288651394547712 SED
-  let str = message.content.replace(`--info `, ``, -1).replace(`-`, ``, -1);
+  let str = message.content.replace(`--praxis `, ``, -1).replace(`-`, ``, -1);
+  // server channel message
+  let strSplit = str.split(``);
+  let guildID = strSplit[0];
+  let channelID = strSplit[1];
+  let msg = strSplit.splice(2, strSplit.length);
+
   let servers = client.guilds;
 
   servers.forEach(guild => {
-    if (guild.id == "332288651394547712") {
+    if (guild.id == guildID) {
       console.log(`we're in the guild`);
       let channels = guild.channels;
+
       channels.forEach(channel => {
-        if (channel.id == "370014438222331915") {
-          channel.send(`Hello`);
+        if (channel.id == channelID) {
+          channel.send(msg);
         }
       });
     }
