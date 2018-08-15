b77daa8f85fbf920a2d7d0a8e4437e67f276567d
Gabriel <gabemorris@outlook.com>
2018-08-14 20:57:23 -0700

    working

--- commands/quote.js
+++ commands/quote.js
@@ -4,9 +4,11 @@ const quotes = require("../include/quotes.json");
 exports.run = (client, message, args) => {
   console.log(`**********Executing quote on ${message.guild.name}**********`);
   let random = Math.floor(Math.random() * quotes.quotes.length);
-  console.log(quotes);
-  console.log(quotes.quotes[random]);
-  console.log(random);
+  let quote = quotes.quotes[random];
 
-  message.channel.send(quotes.quotes[random]).catch(console.error);
+  message.channel
+    .send(
+      `Let us contemplate upon this wise quote by Deepak Chopra:\n\n${quote}`
+    )
+    .catch(console.error);
 };
