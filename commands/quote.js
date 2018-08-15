6c52a06f22277c855fae274e4f8d2e500d6edaa1
Gabriel <gabemorris@outlook.com>
2018-08-14 20:54:06 -0700

    forgot to floor

--- commands/quote.js
+++ commands/quote.js
@@ -3,7 +3,7 @@ const quotes = require("../include/quotes.json");
 // quote message
 exports.run = (client, message, args) => {
   console.log(`**********Executing quote on ${message.guild.name}**********`);
-  let random = Math.random() * quotes.quotes.length;
+  let random = Math.floor(Math.random() * quotes.quotes.length);
   console.log(quotes);
   console.log(quotes.quotes[random]);
   console.log(random);
