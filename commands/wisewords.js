3e4e4c5108b2961efa1aeb60a1c7e6e5c9e15ee7
Gabriel Morris <gabemorris@outlook.com>
2018-08-16 22:04:26 -0700

    Update wisewords to use wisdom-of-chopra + function abstractions

--- commands/wisewords.js
+++ commands/wisewords.js
@@ -1,8 +1,6 @@
 const quotes = require("../include/quotes.json");
 const Discord = require("discord.js");
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-const request = require("request");
+const wiseWords = require("wisdom-of-chopra");
 
 // wisewords message
 exports.run = (client, message, args) => {
@@ -13,54 +11,52 @@ exports.run = (client, message, args) => {
     .replace(`--wisewords`, ``, -1)
     .replace(/-/g, ``, -1);
   let msgArr = msgString.split(` `);
-  console.log(msgArr);
-  let name;
 
-  let randomUserNumber = Math.floor(Math.random() * quotes.names.length);
-  console.log(msgString.length);
-  if (msgString.length < 10) {
-    name = quotes.names[randomUserNumber];
-  } else {
-    name = msgArr.splice(1, msgArr.length).join(` `);
+  // Creat richembed to send
+  const embed = new Discord.RichEmbed()
+    .setAuthor("DoseBot", "https://i.imgur.com/7R8WDwE.png")
+    .setColor("747474")
+    .setFooter(
+      "Please use drugs responsibly",
+      "https://i.imgur.com/7R8WDwE.png"
+    )
+    .setTimestamp()
+    .setURL("http://www.dosebot.org")
+    .setThumbnail(generateThumbnail())
+    .addField("Wise Words", `*${generateQuote()}*\n-${generateName()}`);
+
+  // SEND IT
+  message.channel.send({ embed }).catch(console.error);
+
+  // Function for getting random thumbnail
+  function generateThumbnail() {
+    let thumbArr = [
+      "https://i.imgur.com/WkYo3vd.png",
+      message.author.avatarURL,
+      "https://i.imgur.com/7uI3ri3.png",
+      "https://i.imgur.com/yTVmFhb.png",
+      "https://i.imgur.com/OmuI74P.png",
+      "https://i.imgur.com/q87TndD.png",
+      "https://i.imgur.com/ovxmB8L.png",
+      "https://i.imgur.com/7gf6ERz.jpg"
+    ];
+    let thumbRand = Math.floor(Math.random() * thumbArr.length);
+    return thumbArr[thumbRand];
   }
 
-  let thumbArr = [
-    "https://i.imgur.com/WkYo3vd.png",
-    message.author.avatarURL,
-    "https://i.imgur.com/7uI3ri3.png",
-    "https://i.imgur.com/yTVmFhb.png",
-    "https://i.imgur.com/OmuI74P.png",
-    "https://i.imgur.com/q87TndD.png",
-    "https://i.imgur.com/ovxmB8L.png",
-    "https://i.imgur.com/7gf6ERz.jpg"
-  ];
-  let thumbRand = Math.floor(Math.random() * thumbArr.length);
-  let thumbnail = thumbArr[thumbRand];
+  // Function for getting random name
+  function generateName() {
+    let randomUserNumber = Math.floor(Math.random() * quotes.names.length);
 
-  request("http://wisdomofchopra.com/iframe.php", function(
-    error,
-    response,
-    html
-  ) {
-    if (!error && response.statusCode == 200) {
-      const dom = new JSDOM(html, { runScripts: `dangerously` });
-      let quote = dom.window.document.querySelector(`h2`).textContent;
-
-      const embed = new Discord.RichEmbed()
-        .setAuthor("DoseBot", "https://i.imgur.com/7R8WDwE.png")
-        .setColor("747474")
-        .setFooter(
-          "Please use drugs responsibly",
-          "https://i.imgur.com/7R8WDwE.png"
-        )
-        .setTimestamp()
-        .setURL("http://www.dosebot.org")
-        .setThumbnail(thumbnail)
-        .addField("Wise Words", `*${quote}*\n-${name}`);
-
-      message.channel.send({ embed }).catch(console.error);
+    if (msgString.length < 10) {
+      return quotes.names[randomUserNumber];
     } else {
-      message.channel.send(`Error: The universe's wisdom processor is offline`);
+      return msgArr.splice(1, msgArr.length).join(` `);
     }
-  });
+  }
+
+  // Function for getting random quote
+  function generateQuote() {
+    return wiseWords.getQuote();
+  }
 };
