142e0f435b758a9d1fbf3833d96a40ef7a124f3a
Gabriel Morris <gabemorris@outlook.com>
2018-08-18 11:03:29 -0700

    oops

--- commands/wisewords.js
+++ commands/wisewords.js
@@ -13,6 +13,7 @@ exports.run = (client, message, args) => {
   let msgArr = msgString.split(` `);
   let users = message.guild.members;
   let randomUser;
+  getRandomUser(quotes);
   console.log(randomUser);
 
   // Creat richembed to send
