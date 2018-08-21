8e901b761c08f033da84d0a4ac2ac5cdf1da9908
Gabriel Morris <gabemorris@outlook.com>
2018-08-21 13:45:45 -0700

    Typo

--- commands/bootcamp.js
+++ commands/bootcamp.js
@@ -8,7 +8,7 @@ exports.run = (client, message, args) => {
   function getTimeRemaining(endtime) {
     let t = Date.parse(endtime) - Date.parse(new Date());
     let seconds = Math.floor((t / 1000) % 60);
-    let minutes = Math.floor((t / (1000 / 60)) % 60);
+    let minutes = Math.floor((t / 1000 / 60) % 60);
     let hours = Math.floor((t / (1000 * 60 * 60)) % 24);
     let days = Math.floor(t / (1000 * 60 * 60 * 24));
     return {
