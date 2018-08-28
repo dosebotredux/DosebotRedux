7a039bd50c55e53742b38c1178f7706347520ef5
Gabriel Morris <gabemorris@outlook.com>
2018-08-28 14:50:52 -0700

    Add avatar command

---
+++ commands/avatar.js
@@ -0,0 +1,14 @@
+// Avatar message
+exports.run = (client, message, args) => {
+  console.log(`**********Executing avatar on ${message.guild.name}**********`);
+  const avatar = message.author.avatarURL;
+
+  const embed = new Discord.RichEmbed()
+    .setTitle('DoseBot Avatar Service')
+    .setAuthor('DoseBot', 'https://i.imgur.com/7R8WDwE.png')
+    .setColor('747474')
+    .setURL('http://www.dosebot.org')
+    .attachFile(avatar);
+
+  message.channel.send({ embed });
+};
