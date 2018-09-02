eb132da33e1aa5a639e189268db8d8e399386b43
Gabriel Morris <gabemorris@outlook.com>
2018-09-02 13:21:56 -0700

    Test adding pronoun roles Updates #19

--- commands/role.js
+++ commands/role.js
@@ -1,4 +1,4 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 
 // Welcome to role management
 exports.run = (client, message, args) => {
@@ -9,32 +9,32 @@ exports.run = (client, message, args) => {
   // Create array so we can access multiple args
   let substance;
   let dosage;
-  let strArr = str.replace("--role ", "", -1).split(" "); // "[lsd, 50ug]"
+  let strArr = str.replace('--role ', '', -1).split(' '); // "[lsd, 50ug]"
   if (strArr.length === 2) {
     substance = strArr[1];
-    dosage = "";
+    dosage = '';
   } else if (strArr.length === 3) {
     substance = strArr[1];
     dosage = strArr[2];
   } else {
-    substance = " ";
-    dosage = " ";
+    substance = ' ';
+    dosage = ' ';
   }
   // Set desired role
   let desiredRole;
   desiredRole = str
     .toLowerCase()
-    .replace("--role ", "", -1)
-    .replace(/-/g, "", -1); // "role"
+    .replace('--role ', '', -1)
+    .replace(/-/g, '', -1); // "role"
 
-  let desiredRoleArr = desiredRole.split(" ");
+  let desiredRoleArr = desiredRole.split(' ');
   desiredRole = desiredRoleArr[0];
 
-  if (message.guild.id === "251433048896307200") {
-    if (desiredRole === "tripping") {
-      desiredRole = `tripping💓`;
-    } else if (desiredRole === "baked") {
-      desiredRole = `baked🔥`;
+  if (message.guild.id === '251433048896307200') {
+    if (desiredRole === 'tripping') {
+      desiredRole = 'tripping💓';
+    } else if (desiredRole === 'baked') {
+      desiredRole = 'baked🔥';
     }
   }
 
@@ -44,8 +44,8 @@ exports.run = (client, message, args) => {
   console.log(`Results: ${guildRoles.find(role => role.name === desiredRole)}`);
 
   // Checks to see if the desiredRole is equal to any role object's name property
-  if (!!guildRoles.find(role => role.name.toLowerCase() === desiredRole)) {
-    console.log("Guild has desired role");
+  if (guildRoles.find(role => role.name.toLowerCase() === desiredRole)) {
+    console.log('Guild has desired role');
     // Finds the guild's equivalent of the desiredRole
     let desiredGuildRole = guildRoles.find(
       role => role.name.toLowerCase() === desiredRole
@@ -55,32 +55,34 @@ exports.run = (client, message, args) => {
     // This system prevents assigning of roles outside of intoxicated ones
     // TODO - make this a switch or something
     // Currently servers must use these named roles
-    if (desiredGuildRole.name.toLowerCase() === "tripping") {
-      console.log(`Calling toggleRole`);
+    if (desiredGuildRole.name.toLowerCase() === 'tripping') {
+      console.log('Calling toggleRole');
       toggleRole(desiredGuildRole, author, substance, dosage);
-    } else if (desiredGuildRole.name.toLowerCase() === "stimmed") {
+    } else if (desiredGuildRole.name.toLowerCase() === 'stimmed') {
       toggleRole(desiredGuildRole, author, substance, dosage);
-    } else if (desiredGuildRole.name.toLowerCase() === "barred") {
+    } else if (desiredGuildRole.name.toLowerCase() === 'barred') {
       toggleRole(desiredGuildRole, author, substance, dosage);
-    } else if (desiredGuildRole.name.toLowerCase() === "nodding") {
+    } else if (desiredGuildRole.name.toLowerCase() === 'nodding') {
       toggleRole(desiredGuildRole, author, substance, dosage);
-    } else if (desiredGuildRole.name.toLowerCase() === "drunk") {
+    } else if (desiredGuildRole.name.toLowerCase() === 'drunk') {
       toggleRole(desiredGuildRole, author, substance, dosage);
-    } else if (desiredGuildRole.name.toLowerCase() === "dissod") {
+    } else if (desiredGuildRole.name.toLowerCase() === 'dissod') {
       toggleRole(desiredGuildRole, author, substance, dosage);
-    } else if (desiredGuildRole.name.toLowerCase() === "rolling") {
+    } else if (desiredGuildRole.name.toLowerCase() === 'rolling') {
       toggleRole(desiredGuildRole, author, substance, dosage);
-    } else if (desiredGuildRole.name.toLowerCase() === "stoned") {
+    } else if (desiredGuildRole.name.toLowerCase() === 'stoned') {
       toggleRole(desiredGuildRole, author, substance, dosage);
-    } else if (desiredGuildRole.name.toLowerCase() === "hungover") {
+    } else if (desiredGuildRole.name.toLowerCase() === 'hungover') {
       toggleRole(desiredGuildRole, author, substance, dosage);
-    } else if (desiredGuildRole.name.toLowerCase() === "delirious") {
+    } else if (desiredGuildRole.name.toLowerCase() === 'delirious') {
       toggleRole(desiredGuildRole, author, substance, dosage);
-    } else if (desiredGuildRole.name.toLowerCase() === "altered") {
+    } else if (desiredGuildRole.name.toLowerCase() === 'altered') {
       toggleRole(desiredGuildRole, author, substance, dosage);
-    } else if (desiredGuildRole.name.toLowerCase() === "baked🔥") {
+    } else if (desiredGuildRole.name.toLowerCase() === 'baked🔥') {
       toggleRole(desiredGuildRole, author, substance, dosage);
-    } else if (desiredGuildRole.name.toLowerCase() === "tripping💓") {
+    } else if (desiredGuildRole.name.toLowerCase() === 'tripping💓') {
+      toggleRole(desiredGuildRole, author, substance, dosage);
+    } else if (desiredGuildRole.name.toLowerCase() === 'he/him') {
       toggleRole(desiredGuildRole, author, substance, dosage);
     } else {
       // Send message stating role cannot be assigned
@@ -90,7 +92,7 @@ exports.run = (client, message, args) => {
     }
   } else {
     // Send a message stating role does not exist on server
-    console.log("Guild does not have desired role");
+    console.log('Guild does not have desired role');
     message.channel.send(`Error: Guild does not have **${desiredRole}** role`);
   }
   //// Functions
@@ -98,13 +100,13 @@ exports.run = (client, message, args) => {
   function toggleRole(roleToApply, author, substance, dosage) {
     console.log(`${substance} ${dosage}`);
     var nickName = author.displayName;
-    if (substance !== " ") {
+    if (substance !== ' ') {
       var nickNameModifier = ` | ${substance} ${dosage}`;
     } else {
-      var nickNameModifier = " ";
+      var nickNameModifier = ' ';
     }
     // Conditional to determine whether user has role
-    if (!!author.roles.find(role => role.name === roleToApply.name)) {
+    if (author.roles.find(role => role.name === roleToApply.name)) {
       // Restores nickname
       restoreNickName();
       // Removes role
@@ -118,7 +120,7 @@ exports.run = (client, message, args) => {
 
     function assignRole(roleToApply, author) {
       // Log role and author and add role
-      console.log(`we're in addrole`);
+      console.log('we\'re in addrole');
       author.addRole(roleToApply.id).catch(console.error);
       console.log(`Added ${desiredRole} to <@${message.author.id}>`);
       // Send message to channel
@@ -152,7 +154,7 @@ exports.run = (client, message, args) => {
     }
     function setTripNickName(nick, modifier) {
       // Psy Experience
-      if (message.guild.id === "335167514961248256") {
+      if (message.guild.id === '335167514961248256') {
         nickName = message.member.displayName;
         if (nick !== undefined && modifier !== undefined) {
           console.log(`Nickname: ${nick} ${modifier}`);
@@ -160,31 +162,31 @@ exports.run = (client, message, args) => {
             .setNickname(`${nick} ${modifier}`)
             .catch(console.error);
         } else {
-          console.log(`Nickname is undefined, not modifying name`);
+          console.log('Nickname is undefined, not modifying name');
         }
       }
     }
     function restoreNickName() {
       // Psy Experience
-      if (message.guild.id === "335167514961248256") {
+      if (message.guild.id === '335167514961248256') {
         let currentNick = message.member.displayName;
         console.log(currentNick);
-        if (currentNick.includes("|")) {
-          let currentNickArr = currentNick.split("");
+        if (currentNick.includes('|')) {
+          let currentNickArr = currentNick.split('');
           let nickToRestoreArr = [];
-          let indexOfSeparator = currentNickArr.indexOf("|");
+          let indexOfSeparator = currentNickArr.indexOf('|');
 
           for (let i = 0; i < indexOfSeparator; i++) {
             const letter = currentNickArr[i];
             nickToRestoreArr.push(letter);
           }
 
-          let nickToRestore = nickToRestoreArr.join("");
+          let nickToRestore = nickToRestoreArr.join('');
 
           console.log(`Restoring original nickname: ${nickToRestore}`);
           message.member.setNickname(nickToRestore).catch(console.error);
         } else {
-          console.log(`Not restoring nickname, no | detected`);
+          console.log('Not restoring nickname, no | detected');
         }
       }
     }
