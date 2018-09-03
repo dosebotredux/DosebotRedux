4a9c873299676d7bf57cc242737c2dc783db923a
Gabriel Morris <gabemorris@outlook.com>
2018-09-02 22:10:43 -0700

    Logging for praxis

--- commands/memes/sfw/praxis.js
+++ commands/memes/sfw/praxis.js
@@ -1,32 +1,36 @@
 // praxis
 exports.run = (client, message, args) => {
-  console.log(`**********Executing praxis**********`);
-  let str = message.content.replace(`--praxis `, ``, -1).replace(`-`, ``, -1);
+  console.log('**********Executing praxis**********');
+  let str = message.content.replace('--praxis ', '', -1).replace('-', '', -1);
   // server channel message
-  let strSplit = str.split(` `);
+  let strSplit = str.split(' ');
 
   let guildID;
   let channelID;
   let msg;
 
-  if (typeof strSplit[0] === `number`) {
-    console.log(`num`);
+  if (typeof strSplit[0] === 'number') {
+    console.log('num');
     guildID = strSplit[0];
     channelID = strSplit[1];
-    msg = strSplit.splice(1, strSplit.length).join(` `);
-  } else if (strSplit[0] === `praxis`) {
-    console.log(`prax`);
-    guildID = `350132819307003905`;
-    channelID = `433004930433810442`;
-    msg = strSplit.splice(1, strSplit.length).join(` `);
-  } else if (strSplit[0] === `SED`) {
-    console.log(`SED`);
-    guildID = `332288651394547712`;
-    channelID = `332288651394547712`;
-    msg = strSplit.splice(1, strSplit.length).join(` `);
+    msg = strSplit.splice(1, strSplit.length).join(' ');
+  } else if (strSplit[0] === 'praxis') {
+    console.log('prax');
+    guildID = '350132819307003905';
+    channelID = '433004930433810442';
+    msg = strSplit.splice(1, strSplit.length).join(' ');
+  } else if (strSplit[0] === 'SED') {
+    console.log('SED');
+    guildID = '332288651394547712';
+    channelID = '332288651394547712';
+    msg = strSplit.splice(1, strSplit.length).join(' ');
   }
 
-  console.log(`Guild: ${guildID} Channel: ${channelID} Message: ${msg}`);
+  console.log(
+    `Author: ${
+      message.author.displayName
+    } Guild: ${guildID} Channel: ${channelID} Message: ${msg}`
+  );
 
   let servers = client.guilds;
 
