648fbe0cbf0fc459a9e7a20ee8a675b8572b5ca3
Gabriel Morris <gabemorris@outlook.com>
2018-09-02 20:30:10 -0700

    Modularize role.js Updates #21

--- commands/role.js
+++ commands/role.js
@@ -1,6 +1,7 @@
 // Role message
 exports.run = (client, message, args) => {
   console.log(`**********Executing Role on ${message.guild.name}**********`);
+  const Roles = require('../include/roles.js');
 
   const author = message.member; // author object
   const str = message.content; // "--role role lsd"
@@ -34,65 +35,26 @@ exports.run = (client, message, args) => {
   desiredRole = desiredRoleArr[0];
 
   // Have an object of servers with role exceptions
-  const roleExceptions = {
-    // Psychonauts exceptions
-    '251433048896307200': {
-      tripping: 'tripping💓',
-      baked: 'baked🔥'
-    },
-    // Praxis exceptions
-    '350132819307003905': {
-      any: 'any pronouns'
-    }
-  };
+  const roleExceptions = Roles.getRoleExceptions();
 
   // Have an array of acceptable roles to apply
-  const acceptableTemporaryRoles = [
-    'tripping',
-    'stimmed',
-    'barred',
-    'nodding',
-    'drunk',
-    'dissod',
-    'rolling',
-    'stoned',
-    'hungover',
-    'delirious',
-    'altered',
-    'baked🔥',
-    'tripping💓'
-  ];
+  const acceptableTemporaryRoles = Roles.getTemporaryRoles();
 
   // Have an array of acceptable permanent roles to apply
-  const acceptablePermanentRoles = [
-    'he/him',
-    'she/her',
-    'they/them',
-    'any pronouns',
-    've/ver',
-    'e/em/eir',
-    'earthling',
-    'pup/pupself',
-    'shi/hir',
-    'kit/kitself',
-    'fel/feline',
-    'it/its',
-    'per/perself',
-    'meow',
-    'rib/ribbit'
-  ];
+  const acceptablePermanentRoles = Roles.getPermanentRoles();
 
   // Check to see if current guild has exceptions
-  if (checkIfGuildHasRoleExceptions(message.guild.id)) {
+  if (Roles.checkIfGuildHasRoleExceptions(message.guild.id)) {
     console.log('exception detected');
     // Set role to either exception or original request role
-    desiredRole = returnGuildHasDesiredRoleException(
+    desiredRole = Role.returnGuildHasDesiredRoleException(
       message.guild.id,
       desiredRole
     );
   }
 
   console.log(`Desired role: ${desiredRole}`);
+
   // Log the result of a find operation on the desired role
   console.log(
     `Results: ${guildRoles.find(
@@ -101,17 +63,19 @@ exports.run = (client, message, args) => {
   );
 
   // Checks to see if the desiredRole is equal to any role object's name property
-  if (checkIfGuildHasDesiredRole(guildRoles, desiredRole)) {
+  if (Role.checkIfGuildHasDesiredRole(guildRoles, desiredRole)) {
     // Capture the desired guild role
-    const desiredGuildRole = returnDesiredGuildRoleSnowflake(
+    const desiredGuildRole = Role.returnDesiredGuildRoleSnowflake(
       guildRoles,
       desiredRole
     );
     // Now that we have the desired guild role snowflake we can check if its temporary or permanent
-    if (checkIfRoleIsTemporary(desiredRole, acceptableTemporaryRoles)) {
+    if (Role.checkIfRoleIsTemporary(desiredRole, acceptableTemporaryRoles)) {
       console.log('Role is temporary');
       toggleRole(false, desiredGuildRole, author, substance, dosage);
-    } else if (checkIfRoleIsPermanent(desiredRole, acceptablePermanentRoles)) {
+    } else if (
+      Role.checkIfRoleIsPermanent(desiredRole, acceptablePermanentRoles)
+    ) {
       console.log('Role is permanent');
       toggleRole(true, desiredGuildRole, author, substance, dosage);
     }
@@ -123,85 +87,19 @@ exports.run = (client, message, args) => {
     );
   }
 
-  // Check to see if the current server has role exceptions
-  function checkIfGuildHasRoleExceptions(guildID) {
-    if (roleExceptions[guildID]) {
-      console.log('Guild has role exceptions');
-      return true;
-    }
-    return false;
-  }
-
-  // Function for checking and returning guild role exception
-  function returnGuildHasDesiredRoleException(guildID, desiredRole) {
-    if (roleExceptions[guildID][desiredRole]) {
-      console.log('returning from exception list');
-      console.log(`returning ${roleExceptions[guildID][desiredRole]}`);
-      return roleExceptions[guildID][desiredRole];
-    }
-    console.log('returning original desiredRole');
-    return desiredRole;
-  }
-
-  // Function for checking if guild has the desired role
-  function checkIfGuildHasDesiredRole(guildRolesSnowflake, desiredRole) {
-    console.log('Checking if guild has desired role');
-    if (
-      guildRolesSnowflake.find(role => role.name.toLowerCase() === desiredRole)
-    ) {
-      console.log('Guild has desired role');
-      return true;
-    }
-    console.log('Guild does not have desired role');
-    return false;
-  }
-
-  // Function for returning desired guild role snowflake
-  function returnDesiredGuildRoleSnowflake(guildRolesSnowflake, desiredRole) {
-    return guildRolesSnowflake.find(
-      role => role.name.toLowerCase() === desiredRole
-    );
-  }
-
-  // Function for checking if role is temporary
-  function checkIfRoleIsTemporary(desiredRole, temporaryArray) {
-    let roleIsTemporary = false;
-    temporaryArray.forEach(role => {
-      if (role === desiredRole) {
-        console.log('Role is temporary and found');
-        roleIsTemporary = true;
-      }
-    });
-    console.log(`Desired role: ${desiredRole} - Temporary: ${roleIsTemporary}`);
-    return roleIsTemporary;
-  }
-
-  // Function for checking if a role is permanent
-  function checkIfRoleIsPermanent(desiredRole, permanentArray) {
-    let roleIsPermanent = false;
-    permanentArray.forEach(role => {
-      if (role === desiredRole) {
-        console.log('Role is permanent and found');
-        roleIsPermanent = true;
-      }
-    });
-    console.log(`Desired role: ${desiredRole} - Permanent: ${roleIsPermanent}`);
-    return roleIsPermanent;
-  }
-
   // Function for toggling a role
   function toggleRole(isPermanent, roleToApply, author, substance, dosage) {
     console.log(`In toggleRole Author is: ${author}`);
     if (isPermanent) {
       console.log('toggling permanent role');
       // Conditional to determine whether user has role
-      if (checkIfUserHasRole(author, roleToApply)) {
+      if (Role.checkIfUserHasRole(author, roleToApply)) {
         console.log('Author currently has role');
 
-        unassignRole(roleToApply, author);
+        Role.unassignRole(roleToApply, author);
       } else {
         console.log('Author does not currently have role');
-        assignRole(roleToApply, author, isPermanent);
+        Role.assignRole(roleToApply, author, isPermanent);
       }
     } else if (!isPermanent) {
       console.log('toggling temporary role');
@@ -216,106 +114,19 @@ exports.run = (client, message, args) => {
       }
 
       // Conditional to determine whether user has role
-      if (checkIfUserHasRole(author, roleToApply)) {
+      if (Role.checkIfUserHasRole(author, roleToApply)) {
         console.log('Author currently has role');
         // Restores nickname
-        restoreNickname();
+        Role.restoreNickname();
         // Removes role
-        unassignRole(roleToApply, author);
+        Role.unassignRole(roleToApply, author);
       } else {
         console.log('Author does not currently have role');
         // Sets trip nickname
-        setTripNickName(nickName, nickNameModifier);
+        Role.setTripNickName(nickName, nickNameModifier);
         // Adds role
-        assignRole(roleToApply, author, isPermanent);
+        Role.assignRole(roleToApply, author, isPermanent);
       }
     }
-  }
-
-  // Function for checking if a user has a role
-  function checkIfUserHasRole(author, roleToApply) {
-    console.log(`In checkIfUserHasRole Author is: ${author}`);
-
-    if (author.roles.find(role => role.name === roleToApply.name)) {
-      return true;
-    }
-    return false;
-  }
-
-  // Function for restoring nickname
-  function restoreNickname() {
-    // Only set nickname on Psychedelic Experience
-    if (message.guild.id === '335167514961248256') {
-      let currentNick = message.member.displayName;
-      console.log(`Current nickname: ${currentNick}`);
-
-      if (currentNick.includes('|')) {
-        let currentNickArr = currentNick.split('');
-        let nickToRestoreArr = [];
-        let indexOfSeparator = currentNickArr.indexOf('|');
-
-        for (let i = 0; i < indexOfSeparator; i++) {
-          const letter = currentNickArr[i];
-          nickToRestoreArr.push(letter);
-        }
-        let nickToRestore = nickToRestoreArr.join('');
-        console.log(`Restoring original nickname: ${nickToRestore}`);
-        message.member.setNickname(nickToRestore).catch(console.error);
-      }
-    } else {
-      console.log('Not restoring nickname, no | detected');
-    }
-  }
-
-  // Function for setting a trip nickname
-  function setTripNickName(nick, nickNameModifier) {
-    // Only set nickname on Psychedelic Experience
-    if (message.guild.id === '335167514961248256') {
-      nickName = message.member.displayName;
-      if (nick !== undefined && nickNameModifier !== undefined) {
-        console.log(`Nickname: ${nick} ${nickNameModifier}`);
-        message.member
-          .setNickname(`${nick} ${nickNameModifier}`)
-          .catch(console.error);
-      } else {
-        console.log('Nickname is undefined, not modifying name');
-      }
-    }
-  }
-
-  // Function for adding a role
-  function assignRole(roleToApply, author, isPermanent) {
-    console.log('were in assignRole');
-    author.addRole(roleToApply.id).catch(console.error);
-    console.log(`Added ${desiredRole} to <@${message.author.id}>`);
-    // Send message to channel
-    message.channel.send(`Added **${desiredRole}** to <@${message.author.id}>`);
-
-    // Check if role is permanent and remove only if it is not
-    if (!isPermanent) {
-      const delay = duration =>
-        new Promise(resolve => setTimeout(resolve, duration));
-      // Delay for 8 hours then remove role
-      const asyncFunc = author => {
-        delay(28800000).then(() => {
-          // 28800000ms
-          console.log(
-            `Removed **${roleToApply.name}** from ${author.displayName}`
-          );
-          unassignRole(roleToApply, author);
-          restoreNickname();
-        });
-      };
-      asyncFunc(author);
-    }
-  }
-
-  // Function for unassigning role
-  function unassignRole(roleToApply, author) {
-    console.log(`Removed **${desiredRole}** from ${author.displayName}`);
-    author.removeRole(roleToApply.id).catch(console.error);
-    message.channel.send(
-      `Removed **${desiredRole}** from <@${message.author.id}>`
-    );
   }
 };
--- include/roles.js
+++ include/roles.js
@@ -34,6 +34,19 @@ const acceptablePermanentRoles = [
   'rib/ribbit'
 ];
 
+// Have an object of servers with role exceptions
+const roleExceptions = {
+  // Psychonauts exceptions
+  '251433048896307200': {
+    tripping: 'tripping💓',
+    baked: 'baked🔥'
+  },
+  // Praxis exceptions
+  '350132819307003905': {
+    any: 'any pronouns'
+  }
+};
+
 function getTemporaryRoles() {
   return acceptableTemporaryRoles;
 }
@@ -42,5 +55,174 @@ function getPermanentRoles() {
   return acceptablePermanentRoles;
 }
 
+function getRoleExceptions() {
+  return roleExceptions;
+}
+
+// Check to see if the current server has role exceptions
+function checkIfGuildHasRoleExceptions(guildID) {
+  if (roleExceptions[guildID]) {
+    console.log('Guild has role exceptions');
+    return true;
+  }
+  return false;
+}
+
+// Function for checking if guild has the desired role
+function checkIfGuildHasDesiredRole(guildRolesSnowflake, desiredRole) {
+  console.log('Checking if guild has desired role');
+  if (
+    guildRolesSnowflake.find(role => role.name.toLowerCase() === desiredRole)
+  ) {
+    console.log('Guild has desired role');
+    return true;
+  }
+  console.log('Guild does not have desired role');
+  return false;
+}
+
+// Function for returning desired guild role snowflake
+function returnDesiredGuildRoleSnowflake(guildRolesSnowflake, desiredRole) {
+  return guildRolesSnowflake.find(
+    role => role.name.toLowerCase() === desiredRole
+  );
+}
+
+// Function for checking if role is temporary
+function checkIfRoleIsTemporary(desiredRole, temporaryArray) {
+  let roleIsTemporary = false;
+  temporaryArray.forEach(role => {
+    if (role === desiredRole) {
+      console.log('Role is temporary and found');
+      roleIsTemporary = true;
+    }
+  });
+  console.log(`Desired role: ${desiredRole} - Temporary: ${roleIsTemporary}`);
+  return roleIsTemporary;
+}
+
+// Function for checking if a role is permanent
+function checkIfRoleIsPermanent(desiredRole, permanentArray) {
+  let roleIsPermanent = false;
+  permanentArray.forEach(role => {
+    if (role === desiredRole) {
+      console.log('Role is permanent and found');
+      roleIsPermanent = true;
+    }
+  });
+  console.log(`Desired role: ${desiredRole} - Permanent: ${roleIsPermanent}`);
+  return roleIsPermanent;
+}
+
+// Function for checking and returning guild role exception
+function returnGuildHasDesiredRoleException(guildID, desiredRole) {
+  if (roleExceptions[guildID][desiredRole]) {
+    console.log('returning from exception list');
+    console.log(`returning ${roleExceptions[guildID][desiredRole]}`);
+    return roleExceptions[guildID][desiredRole];
+  }
+  console.log('returning original desiredRole');
+  return desiredRole;
+}
+
+// Function for restoring nickname
+function restoreNickname() {
+  // Only set nickname on Psychedelic Experience
+  if (message.guild.id === '335167514961248256') {
+    let currentNick = message.member.displayName;
+    console.log(`Current nickname: ${currentNick}`);
+
+    if (currentNick.includes('|')) {
+      let currentNickArr = currentNick.split('');
+      let nickToRestoreArr = [];
+      let indexOfSeparator = currentNickArr.indexOf('|');
+
+      for (let i = 0; i < indexOfSeparator; i++) {
+        const letter = currentNickArr[i];
+        nickToRestoreArr.push(letter);
+      }
+      let nickToRestore = nickToRestoreArr.join('');
+      console.log(`Restoring original nickname: ${nickToRestore}`);
+      message.member.setNickname(nickToRestore).catch(console.error);
+    }
+  } else {
+    console.log('Not restoring nickname, no | detected');
+  }
+}
+
+// Function for unassigning role
+function unassignRole(roleToApply, author) {
+  console.log(`Removed **${desiredRole}** from ${author.displayName}`);
+  author.removeRole(roleToApply.id).catch(console.error);
+  message.channel.send(
+    `Removed **${desiredRole}** from <@${message.author.id}>`
+  );
+}
+
+// Function for setting a trip nickname
+function setTripNickName(nick, nickNameModifier) {
+  // Only set nickname on Psychedelic Experience
+  if (message.guild.id === '335167514961248256') {
+    nickName = message.member.displayName;
+    if (nick !== undefined && nickNameModifier !== undefined) {
+      console.log(`Nickname: ${nick} ${nickNameModifier}`);
+      message.member
+        .setNickname(`${nick} ${nickNameModifier}`)
+        .catch(console.error);
+    } else {
+      console.log('Nickname is undefined, not modifying name');
+    }
+  }
+}
+
+// Function for checking if a user has a role
+function checkIfUserHasRole(author, roleToApply) {
+  console.log(`In checkIfUserHasRole Author is: ${author}`);
+
+  if (author.roles.find(role => role.name === roleToApply.name)) {
+    return true;
+  }
+  return false;
+}
+
+// Function for adding a role
+function assignRole(roleToApply, author, isPermanent) {
+  console.log('were in assignRole');
+  author.addRole(roleToApply.id).catch(console.error);
+  console.log(`Added ${desiredRole} to <@${message.author.id}>`);
+  // Send message to channel
+  message.channel.send(`Added **${desiredRole}** to <@${message.author.id}>`);
+
+  // Check if role is permanent and remove only if it is not
+  if (!isPermanent) {
+    const delay = duration =>
+      new Promise(resolve => setTimeout(resolve, duration));
+    // Delay for 8 hours then remove role
+    const asyncFunc = author => {
+      delay(28800000).then(() => {
+        // 28800000ms
+        console.log(
+          `Removed **${roleToApply.name}** from ${author.displayName}`
+        );
+        unassignRole(roleToApply, author);
+        restoreNickname();
+      });
+    };
+    asyncFunc(author);
+  }
+}
+
 module.exports.getTemporaryRoles = getTemporaryRoles;
 module.exports.getPermanentRoles = getPermanentRoles;
+module.exports.getRoleExceptions = getRoleExceptions;
+module.exports.checkIfGuildHasRoleExceptions = checkIfGuildHasRoleExceptions;
+module.exports.checkIfGuildHasDesiredRole = checkIfGuildHasDesiredRole;
+module.exports.returnDesiredGuildRoleSnowflake = returnDesiredGuildRoleSnowflake;
+module.exports.checkIfRoleIsTemporary = checkIfRoleIsTemporary;
+module.exports.checkIfRoleIsPermanent = checkIfRoleIsPermanent;
+module.exports.returnGuildHasDesiredRoleException = returnGuildHasDesiredRoleException;
+module.exports.restoreNickname = restoreNickname;
+module.exports.unassignRole = unassignRole;
+module.exports.setTripNickName = setTripNickName;
+module.exports.assignRole = assignRole;
+module.exports.checkIfUserHasRole = checkIfUserHasRole;
