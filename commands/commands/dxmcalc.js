e7fe79065979b9ce7db847af05855c8ae7d1a217
Gabriel Morris <gabemorris@outlook.com>
2018-09-03 12:44:53 -0700

    Refactor DXM calc per #26

--- commands/commands/dxmcalc.js
+++ commands/commands/dxmcalc.js
@@ -1,85 +1,37 @@
-const Discord = require("discord.js");
-
-//calc dxm plateau dosages. usage --dxmcalc [weight in pounds]
+// Calc dxm plateau dosages. usage --dxmcalc [weight in pounds]
 exports.run = (client, message, args) => {
+  const Discord = require('discord.js');
+  const DXMCalc = require('../../include/dxmcalc.js');
   console.log(`**********Executing dxmcalc on ${message.guild.name}**********`);
 
-  // message variables
-  var str = message.content;
-  var result = str.split(" ");
+  // Message variables
+  const str = message.content;
+  const result = str.split(' ');
   // parse weight from result
-  var weight = parseFloat(result[result.length - 1]);
+  const weight = parseFloat(result[result.length - 1]);
 
   // check to see if weight is a number and terminate if false
   if (isNaN(weight)) {
     message.channel.send(
-      "**Error:** No weight specified | Usage: --dxmcalc [weight in lbs]"
+      '**Error:** No weight specified | Usage: --dxmcalc [weight in lbs]'
     );
     return;
   }
 
-  // kaylee's formula for dxm weight calculation
-  var calculatedDoseModifier = 2 * getLog(125, weight) - 1;
-
-  // modify dosages by weight
-  var lightMin = Math.floor(100 * calculatedDoseModifier);
-  var lightMaxCommonMin = Math.floor(200 * calculatedDoseModifier);
-  var commonMaxStrongMin = Math.floor(400 * calculatedDoseModifier);
-  var strongMaxHeavy = Math.floor(700 * calculatedDoseModifier);
-  var messages = [];
-
   const embed = new Discord.RichEmbed()
-    .setTitle("DXM Dosage Calculator")
-    .setAuthor("DoseBot", "http://www.dosebot.org/images/dose.png")
-    .setColor("747474")
+    .setTitle('DXM Dosage Calculator')
+    .setAuthor('DoseBot', 'http://www.dosebot.org/images/dose.png')
+    .setColor('747474')
     .setFooter(
-      "Please use drugs responsibly",
-      "http://www.dosebot.org/images/dose.png"
+      'Please use drugs responsibly',
+      'http://www.dosebot.org/images/dose.png'
     )
-    .setThumbnail("https://i.imgur.com/7R8WDwE.png")
+    .setThumbnail('https://i.imgur.com/7R8WDwE.png')
     .setTimestamp()
-    .setURL("http://www.dosebot.org")
-    .addField(
-      "[:scales:] Dosages",
-      "First plateau: " +
-        lightMin +
-        "-" +
-        lightMaxCommonMin +
-        "mg" +
-        "\n" +
-        "Second plateau: " +
-        lightMaxCommonMin +
-        "-" +
-        commonMaxStrongMin +
-        "mg" +
-        "\n" +
-        "Third plateau: " +
-        commonMaxStrongMin +
-        "-" +
-        strongMaxHeavy +
-        "mg" +
-        "\n" +
-        "Fourth plateau: " +
-        strongMaxHeavy +
-        "mg+"
-    )
-    .addField(
-      "[:warning:] Warning",
-      "These recommendations are an approximation, please take into account your own personal tolerance and start with lower dosages. Doses exceeding 1500mg are potentially fatal."
-    )
-    .addField(
-      "[:globe_with_meridians:] Links",
-      "[PsychonautWiki](https://psychonautwiki.org/wiki/DXM)" +
-        "\n" +
-        "[Tripsit](http://drugs.tripsit.me/dxm)" +
-        "\n" +
-        "[Drug combination chart](https://wiki.tripsit.me/images/3/3a/Combo_2.png)"
-    );
+    .setURL('http://www.dosebot.org')
+    .addField('[:scales:] Dosages', DXMCalc.generateDosageField(weight))
+    .addField('[:warning:] Warning', DXMCalc.generateWarningField())
+    .addField('[:globe_with_meridians:] Links', DXMCalc.generateLinksField());
 
   message.channel.send({ embed });
 };
-
-// function for getting log base 125
-function getLog(x, y) {
-  return Math.log(y) / Math.log(x);
-}
---
+++ include/dxmcalc.js
@@ -0,0 +1,49 @@
+module.exports.generateDosageField = generateDosageField;
+module.exports.generateWarningField = generateWarningField;
+module.exports.generateLinksField = generateLinksField;
+
+// Function for calculating dosages
+function calculateDosages(weight) {
+  // kaylee's formula for dxm weight calculation
+  const calculatedDoseModifier = 2 * getLog(125, weight) - 1;
+
+  // Return a dosage object
+  return {
+    lightMin: Math.floor(100 * calculatedDoseModifier),
+    lightMaxCommonMin: Math.floor(200 * calculatedDoseModifier),
+    commonMaxStrongMin: Math.floor(400 * calculatedDoseModifier),
+    strongMaxHeavy: Math.floor(700 * calculatedDoseModifier)
+  };
+}
+
+// Function for getting log base 125
+function getLog(x, y) {
+  return Math.log(y) / Math.log(x);
+}
+
+// Function for generating dosage field
+function generateDosageField(weight) {
+  const dosageObject = calculateDosages(weight);
+  return `**First plateau**: ${dosageObject.lightMin}-${
+    dosageObject.lightMaxCommonMin
+  }mg
+  **Second plateau**: ${dosageObject.lightMaxCommonMin}-${
+  dosageObject.commonMaxStrongMin
+}mg
+  **Third plateau**: ${dosageObject.commonMaxStrongMin}${
+  dosageObject.strongMaxHeavy
+}mg
+  **Fourth plateau**: ${dosageObject.strongMaxHeavy}+mg`;
+}
+
+// Function for generating warning field
+function generateWarningField() {
+  return `These recommendations are an approximation, please take into account your own personal tolerance and start with lower dosages. Doses exceeding 1500mg are potentially fatal.`;
+}
+
+// Function for generating links field
+function generateLinksField() {
+  return `[PsychonautWiki](https://psychonautwiki.org/wiki/DXM)
+  [Tripsit](http://drugs.tripsit.me/dxm)
+  [Drug combination chart](https://wiki.tripsit.me/images/3/3a/Combo_2.png)`;
+}
