fdeff9af025f017bb462445db05e038a7e9e8bc9
Gabriel Morris <gabemorris@outlook.com>
2018-09-10 12:03:13 -0700

    Test logging mod actions

--- bot.js
+++ bot.js
@@ -1,17 +1,17 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
 const token = process.env.DISCORD_API_TOKEN;
-const CommandSystem = require("./command-system.js")();
+const CommandSystem = require('./command-system.js')();
 
 // On ready logic
-client.on("ready", () => {
+client.on('ready', () => {
   // Update game message on launch
   updateGameMessage();
-  console.log("DoseBot is online - beep boop");
+  console.log('DoseBot is online - beep boop');
 });
 
 // Log new guild joins
-client.on("guildCreate", guild => {
+client.on('guildCreate', guild => {
   // Update game message on new server join
   updateGameMessage();
   console.log(
@@ -20,13 +20,24 @@ client.on("guildCreate", guild => {
 });
 
 // Pass messages to the CommandSystem
-client.on("message", message => {
+client.on('message', message => {
   CommandSystem.execute(client, message);
+  // if we've got a message in sed
+  if (message.guild.id === '332288651394547712') {
+    // we need to find the server
+    const server = message.guild;
+    // and find the channel
+    const loggedChannel = server.channels.find(channel => {
+      return channel.id === '488784584138293268';
+    });
+
+    loggedChannel.send('Test');
+  }
 });
 
 //
 CommandSystem.load(function() {
-  console.log("Command system loaded.");
+  console.log('Command system loaded.');
 });
 
 client.login(token);
