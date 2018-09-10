a8dbc27185065f176b04c146ec0d623537a24d21
Gabriel Morris <gabemorris@outlook.com>
2018-09-10 12:28:10 -0700

    stop the madness

--- bot.js
+++ bot.js
@@ -32,10 +32,10 @@ client.on('message', message => {
       return channel.id === '488784584138293268';
     });
 
-    const author = message.author.id;
-    const message = message.content;
-    const loggedMessage = `<@${author}> - ${message}`;
-    console.log(loggedMessage);
+    // const author = message.author.id;
+    // const message = message.content;
+    // const loggedMessage = `<@${author}> - ${message}`;
+    // console.log(loggedMessage);
 
     // loggingChannel.send(
     //   `Author: ${message.author.name} - Message: ${message.content}`
