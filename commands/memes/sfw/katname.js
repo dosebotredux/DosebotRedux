6cf198004528e4590e3601ba23adb9d52c7b9dda
Gabriel Morris <gabemorris@outlook.com>
2018-09-12 12:41:22 -0700

    Fixed error handling

--- commands/memes/sfw/katname.js
+++ commands/memes/sfw/katname.js
@@ -12,11 +12,14 @@ exports.run = (client, message, args) => {
     return member.id === '371151824331210755';
   });
 
-  kat
-    .setNickname(str)
-    .then(message.channel.send(`Updated Kat's nickname to **${str}**`))
-    .catch(err => {
-      console.error(err);
-      message.channel.send('Error: nick: Must be 32 or fewer in length.');
-    });
+  if (str.length < 32) {
+    kat
+      .setNickname(str)
+      .then(message.channel.send(`Updated Kat's nickname to **${str}**`))
+      .catch(err => {
+        console.error(err);
+      });
+  } else {
+    message.channel.send('Error: nick: Must be 32 or fewer in length.');
+  }
 };
