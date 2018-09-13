045d35ee8903e3cc4a6a9feddd06ea28acff5c60
Gabriel Morris <gabemorris@outlook.com>
2018-09-13 11:43:59 -0700

    find quotes

--- commands/commands/findquote.js
+++ commands/commands/findquote.js
@@ -19,9 +19,9 @@ exports.run = (client, message, args) => {
       const db = client.db(dbName);
       const collection = db.collection('quotes');
 
-      const foundQuotes = collection
-        .find({ author: 'Kaylee' })
-        .then(data => console.log(data));
+      const foundQuotes = collection.find({ author: 'Kaylee' });
+
+      foundQuotes.then(data => console.log(data));
       console.log(foundQuotes);
     }
   );
