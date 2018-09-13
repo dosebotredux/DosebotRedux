61c0ae97ad815f58ecb93827b6d043e893aea897
Gabriel Morris <gabemorris@outlook.com>
2018-09-13 06:55:06 -0700

    lowered chance

--- katspeaker.js
+++ katspeaker.js
@@ -118,9 +118,8 @@ module.exports = function KatSpeaker() {
       ];
       const adejectives = ['goddamn', 'fucking', 'miserable', 'worthless'];
       const rand = Math.floor(Math.random() * 100);
-      console.log(`Random: ${rand}`);
 
-      if (rand < 5) {
+      if (rand < 2) {
         const meanThingsRand = Math.floor(Math.random() * meanThings.length);
         const adjectivesRand = Math.floor(Math.random() * adejectives.length);
 
