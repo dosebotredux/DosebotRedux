c345f1e940670ed03fe5fdbb98830e1df53d9873
Gabriel Morris <gabemorris@outlook.com>
2018-09-15 21:04:05 -0700

    improved cocoa meme

--- commands/memes/sfw/cocoa.js
+++ commands/memes/sfw/cocoa.js
@@ -2,9 +2,10 @@
 exports.run = (client, message, args) => {
   console.log(`**********Executing cocoa on ${message.guild.name}**********`);
 
+  message.channel.send('https://i.imgur.com/ZuXH5nP.jpg');
   message.channel
     .send(
-      "Long live the right hand of the revolution, <@278301453620084736>\nThe lightkeeper\nThe brilliant flame\nThe lord of the first ember"
+      'Long live the right hand of the revolution, <@278301453620084736>\nThe lightkeeper\nThe brilliant flame\nThe lord of the first ember'
     )
     .catch(console.error);
 };
