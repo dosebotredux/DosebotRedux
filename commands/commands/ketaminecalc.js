6c49ba1b4c3425dd709f31dd35a7b31576ba70aa
Gabriel Morris <gabemorris@outlook.com>
2018-09-30 14:17:35 -0700

    added metric support for ketaminecalc #40

--- commands/commands/ketaminecalc.js
+++ commands/commands/ketaminecalc.js
@@ -21,7 +21,7 @@ exports.run = (client, message, args) => {
   // check to see if weight is a number and terminate if false
   if (isNaN(weight)) {
     message.channel.send(
-      '**Error:** No weight specified | Usage: --ketaminecalc [weight in lbs]'
+      '**Error:** No weight specified | Usage: --ketaminecalc [weight]<optional: lbs/kg'
     );
     return;
   }
@@ -38,43 +38,87 @@ exports.run = (client, message, args) => {
     .setTimestamp()
     .setURL('http://www.dosebot.org')
     .addField('[:scales:] Dosages')
-    .addField('Insufflated', generateInsufflatedDosages(weight), true)
-    .addField('Intramuscular', generateIntramuscularDosages(weight), true)
-    .addField('Oral', generateOralDosage(weight), true)
-    .addField('Rectal', generateRectalDosage(weight), true);
+    .addField(
+      'Insufflated',
+      generateInsufflatedDosages(weight, weightIsKilos),
+      true
+    )
+    .addField(
+      'Intramuscular',
+      generateIntramuscularDosages(weight, weightIsKilos),
+      true
+    )
+    .addField('Oral', generateOralDosage(weight, weightIsKilos), true)
+    .addField('Rectal', generateRectalDosage(weight, weightIsKilos), true);
 
   message.channel.send({ embed });
 
-  function generateInsufflatedDosages(weight) {
-    return `**Threshold**: ${weight * 0.1}mg
-    **Light**: ${weight * 0.15}mg
-    **Common**: ${weight * 0.3}mg
-    **Strong**: ${weight * 0.5}-${weight * 0.75}mg
-    **K-hole**: ${weight}mg`;
+  function calculateWeight(weight, isKilos) {
+    let weightInLbs;
+
+    if (isKilos) {
+      weightInLbs = Math.floor(weight * 2.2);
+    } else {
+      weightInLbs = weight;
+    }
+
+    return weightInLbs;
   }
 
-  function generateIntramuscularDosages(weight) {
-    return `**Threshold**: ${weight * 0.1}mg
-    **Light**: ${weight * 0.15}mg
-    **Common**: ${weight * 0.2}mg
-    **Strong**: ${weight * 0.5}mg
-    **K-hole**: ${weight * 0.75}mg
-    **Anesthetic**: ${weight}mg`;
+  function generateInsufflatedDosages(weight, isKilos) {
+    const weightInLbs = calculateWeight(weight, isKilos);
+    const dosageArray = [];
+
+    dosageArray.push(`Dosages for: ${weight}${isKilos ? 'kg' : 'lbs'}`);
+
+    dosageArray.push(`**Threshold**: ${weightInLbs * 0.1}mg`);
+    dosageArray.push(`**Light**: ${weightInLbs * 0.15}mg`);
+    dosageArray.push(`**Common**: ${weightInLbs * 0.3}mg`);
+    dosageArray.push(
+      `**Strong**: ${weightInLbs * 0.5}-${weightInLbs * 0.75}mg`
+    );
+    dosageArray.push(`**K-hole**: ${weightInLbs}mg`);
+
+    return dosageArray.join('\n');
   }
 
-  function generateOralDosage(weight) {
-    return `**Threshold**: ${weight * 0.3}mg
-    **Light**: ${weight * 0.6}mg
-    **Common**: ${weight * 0.75}-${weight * 2}mg
-    **Strong**: ${weight * 2}-${weight * 2.5}mg
-    **K-hole**: ${weight * 3}-${weight * 4}mg`;
+  function generateIntramuscularDosages(weight, isKilos) {
+    const weightInLbs = calculateWeight(weight, isKilos);
+    const dosageArray = [];
+
+    dosageArray.push(`**Threshold**: ${weightInLbs * 0.1}mg`);
+    dosageArray.push(`**Light**: ${weightInLbs * 0.15}mg`);
+    dosageArray.push(`**Common**: ${weightInLbs * 0.2}mg`);
+    dosageArray.push(`**Strong**: ${weightInLbs * 0.5}mg`);
+    dosageArray.push(`**K-hole**: ${weightInLbs * 0.75}mg`);
+    dosageArray.push(`**Anesthetic**: ${weightInLbs}mg`);
+
+    return dosageArray.join('\n');
   }
 
-  function generateRectalDosage(weight) {
-    return `**Threshold**: ${weight * 0.3}mg
-    **Light**: ${weight * 0.6}mg
-    **Common**: ${weight * 0.75}-${weight * 2}mg
-    **Strong**: ${weight * 2}-${weight * 2.5}mg
-    **K-hole**: ${weight * 3}-${weight * 4}mg`;
+  function generateOralDosage(weight, isKilos) {
+    const weightInLbs = calculateWeight(weight, isKilos);
+    const dosageArray = [];
+
+    dosageArray.push(`**Threshold**: ${weightInLbs * 0.3}mg`);
+    dosageArray.push(`**Light**: ${weightInLbs * 0.6}mg`);
+    dosageArray.push(`**Common**: ${weightInLbs * 0.75}-${weightInLbs * 2}mg`);
+    dosageArray.push(`**Strong**: ${weightInLbs * 2}-${weightInLbs * 2.5}mg`);
+    dosageArray.push(`**K-hole**: ${weightInLbs * 3}-${weightInLbs * 4}mg`);
+
+    return dosageArray.join('\n');
+  }
+
+  function generateRectalDosage(weight, isKilos) {
+    const weightInLbs = calculateWeight(weight, isKilos);
+    const dosageArray = [];
+
+    dosageArray.push(`**Threshold**: ${weightInLbs * 0.3}mg`);
+    dosageArray.push(`**Light**: ${weightInLbs * 0.6}mg`);
+    dosageArray.push(`**Common**: ${weightInLbs * 0.75}-${weightInLbs * 2}mg`);
+    dosageArray.push(`**Strong**: ${weightInLbs * 2}-${weightInLbs * 2.5}mg`);
+    dosageArray.push(`**K-hole**: ${weightInLbs * 3}-${weightInLbs * 4}mg`);
+
+    return dosageArray.join('\n');
   }
 };
