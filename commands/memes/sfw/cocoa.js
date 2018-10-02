965d7fdc9f476ee0b56e39d80a33d2972328561e
Gabriel Morris <gabemorris@outlook.com>
2018-10-02 13:46:11 -0700

    oopsies

--- commands/memes/sfw/cocoa.js
+++ commands/memes/sfw/cocoa.js
@@ -5,7 +5,7 @@ exports.run = (client, message, args) => {
   message.channel.send('https://i.imgur.com/MLmGTll.jpg');
   message.channel
     .send(
-      'Hail <@278301453620084736>, right hand of the revolution\nThe lightkeeperBotmaster\nDefender of the Northern Realms'
+      'Hail <@278301453620084736>, right hand of the revolution\nThe lightkeeper\nBotmaster\nDefender of the Northern Realms'
     )
     .catch(console.error);
 };
