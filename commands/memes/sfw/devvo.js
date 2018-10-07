3b81b7f0191cf9e5c24cea0c87f40ac76f92a473
Gabriel Morris <gabemorris@outlook.com>
2018-10-07 15:51:11 -0700

    fix typo in devvo

--- commands/memes/sfw/devvo.js
+++ commands/memes/sfw/devvo.js
@@ -6,6 +6,6 @@ exports.run = (client, message, args) => {
     'https://cdn.discordapp.com/attachments/433004930433810442/498207550248845329/591be855eb306c752d821164ac7805cdcf7a7164.png'
   );
   message.channel
-    .send('@118117307514880000 DEVVO MAAAAYYYYTE')
+    .send('<@118117307514880000> DEVVO MAAAAYYYYTE')
     .catch(console.error);
 };
