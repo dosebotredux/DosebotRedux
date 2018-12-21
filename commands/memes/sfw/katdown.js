78124f252cbe92d1a54d177c72a54c816c79d918
Gabriel Morris <gabemorris@outlook.com>
2018-12-21 09:29:25 -0800

    updated katdown;

--- commands/memes/sfw/katdown.js
+++ commands/memes/sfw/katdown.js
@@ -1,9 +1,7 @@
 // Bootcamp motivation message
 exports.run = (client, message, args) => {
-  console.log(
-    `**********Executing bootcamp on ${message.guild.name}**********`
-  );
-  var endOfYear = 'January 4 2019 23:59:59 GMT-0800';
+  console.log(`**********Executing katdown on ${message.guild.name}**********`);
+  const endOfYear = 'January 8 2019 05:45:00 GMT-0800';
 
   function getTimeRemaining(endtime) {
     let t = Date.parse(endtime) - Date.parse(new Date());
@@ -23,7 +21,9 @@ exports.run = (client, message, args) => {
 
   const channelMessage = `${countdown.days} days, ${countdown.hours} hours, ${
     countdown.minutes
-  } minutes, ${countdown.seconds} seconds until KAT MEETUP!`;
+  } minutes, ${
+    countdown.seconds
+  } seconds until KAT MEETUP! <@371151824331210755>`;
 
   message.channel.send(channelMessage).catch(console.error);
 };
