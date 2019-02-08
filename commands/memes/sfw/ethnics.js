bf62c75b0a6ab9a41f758dc12e64f687b11e2257
Gabriel Morris <gabemorris@outlook.com>
2019-02-08 14:18:08 -0800

    per request these will ping kat now

--- commands/memes/sfw/ethnics.js
+++ commands/memes/sfw/ethnics.js
@@ -2,5 +2,7 @@
 exports.run = (client, message, args) => {
   console.log(`**********Executing ethnics on ${message.guild.name}**********`);
 
-  message.channel.send('https://i.imgur.com/Wsi98ev.gif').catch(console.error);
+  message.channel
+    .send('<@371151824331210755> https://i.imgur.com/Wsi98ev.gif')
+    .catch(console.error);
 };
