29c7b7f79d230498a3adb47c095d33c10d952faf
Will LaFrance <wjlafrance@gmail.com>
2019-02-10 20:03:03 -0600

    mascot command attaches asset to post, no imgur link

--- commands/mascot.js
+++ commands/mascot.js
@@ -1,5 +1,5 @@
 exports.run = (client, message, args) => {
-  let response = `Hello <@${ message.author.id }> I'm DoseBot https://i.imgur.com/Rabwdwx.png Nice to meet you ^_^`;
-  message.channel.send(response).catch(console.error);
+  message.channel
+    .send(`Hello <@${ message.author.id }>, I'm DoseBot Redux! Nice to meet you! ^_^`, { files: ["./assets/mascot.png"] } )
+    .catch(console.error);
 };
-
