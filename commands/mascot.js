d63183b564c30fd1f7695d07a370374f27bb6d30
Will LaFrance <wjlafrance@gmail.com>
2019-02-10 21:02:17 -0600

    remove dangling space

--- commands/mascot.js
+++ commands/mascot.js
@@ -1,5 +1,5 @@
 exports.run = (client, message, args) => {
   message.channel
-    .send(`Hello <@${ message.author.id }>, I'm DoseBot Redux! Nice to meet you! ^_^`, { files: ["./assets/mascot.png"] } )
+    .send(`Hello <@${ message.author.id }>, I'm DoseBot Redux! Nice to meet you! ^_^`, { files: ["./assets/mascot.png"] })
     .catch(console.error);
 };
