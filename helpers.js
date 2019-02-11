cee4e7282d1631a43c04dc57ebccdb81e4ef6cd2
Will LaFrance <wjlafrance@gmail.com>
2019-02-10 20:53:41 -0600

    fix attachment URLs

--- helpers.js
+++ helpers.js
@@ -4,9 +4,9 @@ exports.TemplatedRichEmbed = function () {
   return new Discord.RichEmbed()
     .setTimestamp()
     .attachFile("./assets/logo.png")
-    .setAuthor('DoseBot Redux', 'attachments://logo.png')
-    .setThumbnail('attachments://logo.png')
+    .setAuthor('DoseBot Redux', 'attachment://logo.png')
+    .setThumbnail('attachment://logo.png')
     .setColor('747474')
     .setURL('http://www.effectindex.com')
-    .setFooter('Please use drugs responsibly', 'attachments://logo.png')
+    .setFooter('Please use drugs responsibly', 'attachment://logo.png')
 }
