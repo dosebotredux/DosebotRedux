a55bd47632cc338bf7c9f172f9a8fd4a54048982
Will LaFrance <wjlafrance@gmail.com>
2019-02-10 20:54:35 -0600

    about command uses helper, embedded resources

--- commands/about.js
+++ commands/about.js
@@ -1,24 +1,16 @@
 // About message
 const Discord = require('discord.js');
+const Helpers = require('../helpers.js')
 
 exports.run = (client, message, args) => {
-  const embed = new Discord.RichEmbed()
-    .setAuthor('DoseBot Redux', 'https://i.imgur.com/7R8WDwE.png')
-    .setColor('747474')
-    .setFooter(
-      'Please use drugs responsibly',
-      'https://i.imgur.com/7R8WDwE.png'
-    )
-    .setThumbnail('https://i.imgur.com/7R8WDwE.png')
-    .setTimestamp()
-    .setURL('http://www.dosebot.org')
+  const embed = Helpers.TemplatedRichEmbed()
     .addField(
-      'Assistance',
+      'About DoseBot Redux',
       `DoseBot Redux automatically sources dosage, duration, tolerance, and harm reduction information from [PsychonautWiki](http://www.psychonautwiki.org) and [Effect Index](https://effectindex.com).
 
       It was created with the goal of raising awareness of harm reduction best practices, as well as the Subjective Effect Index. For a complete list of commands type \'--help\'.
 
-      For more information about the original Dosebot, see [DoseBot on Effect Index](https://effectindex.com/dosebot) or join the Subjective Effect Documentation [Discord server](https://discord.gg/2J5XBfX).`
+      For more information about the original Dosebot, see [DoseBot on Effect Index](https://effectindex.com/dosebot).`
     );
 
   message.channel.send({ embed });
