7cde678140c602c66a18b715f5b7d2a60b0ec60a
William LaFrance <wjlafrance@gmail.com>
2019-10-21 23:16:14 -0500

    Use included asset for combo chart rather than wiki link

--- commands/combochart.js
+++ commands/combochart.js
@@ -1,8 +1,6 @@
 //tripsit combo chart message
 exports.run = (client, message, args) => {
   message.channel
-    .send(
-      "Drug combination chart: https://wiki.tripsit.me/images/3/3a/Combo_2.png"
-    )
+    .send({ files: ["./assets/combochart.png"] })
     .catch(console.error);
 };
