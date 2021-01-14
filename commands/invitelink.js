6645a014024563a9a66c974bb3c1bb97b133d5c7
William LaFrance <wjlafrance@gmail.com>
2021-01-14 15:44:38 -0600

    Update invite link

--- commands/invitelink.js
+++ commands/invitelink.js
@@ -1,5 +1,5 @@
 exports.run = (client, message, args) => {
   message.channel
-    .send('Want to invite Dosebot Redux to your server? Click this: https://discordapp.com/oauth2/authorize?&client_id=435987984450453505&scope=bot&permissions=268486656')
+    .send('Want to invite Dosebot Redux to your server? Click this: https://discord.com/oauth2/authorize?client_id=799165497710084116&scope=bot&permissions=268815552')
     .catch(console.error);
 }
