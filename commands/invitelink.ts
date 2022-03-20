ab9dfaecab9c0c220908aa6ecb70b7609bd11189
William LaFrance <wjlafrance@gmail.com>
2022-03-20 00:55:29 -0500

    update invitelink with proper scope

--- commands/invitelink.ts
+++ commands/invitelink.ts
@@ -1,5 +1,5 @@
 import Discord from 'discord.js';
 
 export function run(client: Discord.Client, message: Discord.Message, args: string[]) {
-  message.reply('Want to invite Dosebot Redux to your server? Click this: https://discord.com/oauth2/authorize?client_id=799165497710084116&scope=bot&permissions=268815552');
+  message.reply('Want to invite Dosebot Redux to your server? Click this: https://discord.com/api/oauth2/authorize?client_id=799165497710084116&permissions=2416299200&scope=applications.commands%20bot');
 }
